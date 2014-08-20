07a1bd75f0418b87838f8b4f6d80ec25d36866d6
Mike Sands <michael.sands@sa.gov.au>
2014-08-20 15:07:44 +0930

    Ensure FacesAction.validateRequiredFields processes widgets with clientIds that are posted from grids and lists.

--- wildcat-web/src/org/skyve/wildcat/web/faces/FacesAction.java
+++ wildcat-web/src/org/skyve/wildcat/web/faces/FacesAction.java
@@ -88,12 +88,13 @@ public abstract class FacesAction<T> {
     	for (String paramName : requestMap.keySet()) {
     		String paramValue = requestMap.get(paramName);
     		String clientId = paramName;
+    		clientId = clientId.replaceAll(":\\d+:", ":"); // remove the grid indexing used in request parameters
     		if (clientId.endsWith("_input")) { // date columns, check boxes etc
     			clientId = clientId.substring(0, clientId.length() - 6);
     		}
     		if ((paramValue == null) || "".equals(paramValue.trim())) {
-				UIComponent component = findComponentByClientId(fc.getViewRoot(), clientId);
-				if ((component != null) && component.isRendered() && (component instanceof UIInput)) {
+    			UIComponent component = findComponentByClientId(fc.getViewRoot(), clientId);
+    			if ((component != null) && component.isRendered() && (component instanceof UIInput)) {
 					String message = ((UIInput) component).getRequiredMessage();
 					if (message != null) {
 						result = false;
@@ -109,45 +110,49 @@ public abstract class FacesAction<T> {
     }
     
 	public static UIComponent findComponentById(UIComponent base, String id) {
-	    if (id.equals(base.getId()))
-	      return base;
-	  
-	    UIComponent kid = null;
-	    UIComponent result = null;
-	    Iterator<UIComponent> kids = base.getFacetsAndChildren();
-	    while (kids.hasNext()) {
-	      kid = kids.next();
-	      if (id.equals(kid.getId())) {
-	        result = kid;
-	        break;
-	      }
-	      result = findComponentById(kid, id);
-	      if (result != null) {
-	        break;
-	      }
-	    }
-	    return result;
+		if (id.equals(base.getId())) {
+			return base;
+		}
+		
+		UIComponent kid = null;
+		UIComponent result = null;
+		Iterator<UIComponent> kids = base.getFacetsAndChildren();
+		while (kids.hasNext()) {
+			kid = kids.next();
+			if (id.equals(kid.getId())) {
+				result = kid;
+				break;
+			}
+			result = findComponentById(kid, id);
+			if (result != null) {
+				break;
+			}
+		}
+		
+		return result;
 	}
 
 	public static UIComponent findComponentByClientId(UIComponent base, String id) {
-	    if (id.equals(base.getClientId()))
-	      return base;
-	  
-	    UIComponent kid = null;
-	    UIComponent result = null;
-	    Iterator<UIComponent> kids = base.getFacetsAndChildren();
-	    while (kids.hasNext()) {
-	      kid = kids.next();
-	      if (id.equals(kid.getClientId())) {
-	        result = kid;
-	        break;
-	      }
-	      result = findComponentByClientId(kid, id);
-	      if (result != null) {
-	        break;
-	      }
-	    }
-	    return result;
+		if (id.equals(base.getClientId())) {
+			return base;
+		}
+		
+		UIComponent kid = null;
+		UIComponent result = null;
+		Iterator<UIComponent> kids = base.getFacetsAndChildren();
+		while (kids.hasNext()) {
+			kid = kids.next();
+			if (id.equals(kid.getClientId())) {
+				result = kid;
+				break;
+			}
+			result = findComponentByClientId(kid, id);
+			if (result != null) {
+				break;
+			}
+		}
+		
+		return result;
 	}
 
 	public static List<UIComponent> findComponentsByBinding(UIComponent base, String binding) {
