2ae317ad938e19d818b7433c755c101f27aa7850
Mike Sands <mike.sands@bizhub.com.au>
2016-04-05 15:01:00 +0930

    admin.DataMaintenance - Export - Return a blank spreadsheet if no module/document is selected before exporting.

--- wildcat-ee/src/skyve/modules/admin/DataMaintenance/actions/DataMaintenanceExportAction.java
+++ wildcat-ee/src/skyve/modules/admin/DataMaintenance/actions/DataMaintenanceExportAction.java
@@ -1,35 +1,26 @@
 package modules.admin.DataMaintenance.actions;
 
+import org.skyve.EXT;
+import org.skyve.bizport.BizPortWorkbook;
+import org.skyve.metadata.controller.BizExportAction;
+import org.skyve.web.WebContext;
+
 import modules.ModulesUtil;
 import modules.admin.domain.DataMaintenance;
-
-import org.skyve.CORE;
-import org.skyve.bizport.BizPortWorkbook;
-import org.skyve.domain.messages.Message;
-import org.skyve.domain.messages.ValidationException;
-import org.skyve.metadata.controller.BizExportAction;
-import org.skyve.metadata.customer.Customer;
-import org.skyve.metadata.model.document.Document;
-import org.skyve.metadata.module.Module;
-import org.skyve.web.WebContext;
 
 public class DataMaintenanceExportAction extends BizExportAction {
 	private static final long serialVersionUID = -4559826499532491074L;
 
 	@Override
 	public BizPortWorkbook bizExport(WebContext webContext) throws Exception {
-
 		DataMaintenance d = (DataMaintenance) webContext.getCurrentBean();
 		String modocName = d.getModDocName();
-		if (modocName == null) {
-			Customer customer = CORE.getPersistence().getUser().getCustomer();
-			Module module = customer.getModule(DataMaintenance.MODULE_NAME);
-			Document document = module.getDocument(customer, DataMaintenance.DOCUMENT_NAME);
 
-			StringBuilder sb = new StringBuilder(64);
-			sb.append("Select ").append(document.getAttribute(DataMaintenance.modDocNamePropertyName).getDisplayName());
-			
-			throw new ValidationException(new Message(DataMaintenance.modDocNamePropertyName, sb.toString()));
+		// If nothing selected, just return a blank spreadsheet
+		if (modocName == null) {
+			BizPortWorkbook result = EXT.newBizPortWorkbook(false);
+			result.materialise();
+			return result;
 		}
 
 		String[] refs = modocName.split("\\.");
