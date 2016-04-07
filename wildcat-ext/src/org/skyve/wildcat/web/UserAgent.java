05878b7b93a0c3860014fc5312d53c07f5ca2095
Mike Sands <michael.sands@sa.gov.au>
2016-04-07 12:19:12 +0930

    Turf sourceforge UA Detector as it is no longer active and switch to UserAgentUtils.

--- wildcat-ext/src/org/skyve/wildcat/web/UserAgent.java
+++ wildcat-ext/src/org/skyve/wildcat/web/UserAgent.java
@@ -5,9 +5,8 @@ import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 
-import net.sf.uadetector.ReadableUserAgent;
-import net.sf.uadetector.UserAgentStringParser;
-import net.sf.uadetector.service.UADetectorServiceFactory;
+import eu.bitwalker.useragentutils.DeviceType;
+import eu.bitwalker.useragentutils.OperatingSystem;
 
 public class UserAgent {
 	/**
@@ -40,84 +39,33 @@ public class UserAgent {
 
 		UserAgentType result = typeCache.get(agentString);
 		if (result == null) {
-			UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
-			ReadableUserAgent agent = parser.parse(agentString);
+			result = UserAgentType.other;
 			
-			switch (agent.getDeviceCategory().getCategory()) {
-				case PERSONAL_COMPUTER:
-					result = UserAgentType.desktop;
-					break;
-				case TABLET:
-					result = UserAgentType.tablet;
-					break;
-				case SMARTPHONE:
-				case WEARABLE_COMPUTER:
-					result = UserAgentType.phone;
-					break;
-				default:
-					result = UserAgentType.other;
-			}
-
-			// Manually detect galaxy tabs - they are not phones
-			if ( UserAgentType.phone.equals(result) && agentString.contains("SCH-I800")) {
-				result = UserAgentType.tablet;
+			eu.bitwalker.useragentutils.UserAgent agent = eu.bitwalker.useragentutils.UserAgent.parseUserAgentString(agentString);
+			if (agent != null) {
+				OperatingSystem os = agent.getOperatingSystem();
+				if (os != null) {
+					DeviceType dt = os.getDeviceType();
+					if (dt != null) {
+						switch (dt) {
+						case COMPUTER:
+							result = UserAgentType.desktop;
+							break;
+						case TABLET:
+							result = UserAgentType.tablet;
+							break;
+						case MOBILE:
+						case WEARABLE:
+							result = UserAgentType.phone;
+							break;
+						default:
+							result = UserAgentType.other;
+						}
+					}
+				}
 			}
 
 			typeCache.put(agentString, result);
-		}
-		
-		return result;
-	}
-	
-	private static Map<String, OperatingSystem> osCache = new TreeMap<>();
-
-	public static enum OperatingSystem {
-		ANDROID, BLACKBERRY, IOS, LINUX, MACOS, UNIX, WINDOWS, OTHER
-	}
-	
-	public static OperatingSystem getOS(HttpServletRequest request) {
-System.out.println("GET OS");
-		String agentString = request.getHeader("User-Agent");
-		if (agentString == null) {
-			agentString = "";
-		}
-
-		OperatingSystem result = osCache.get(agentString);
-		if (result == null) {
-			UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
-			ReadableUserAgent agent = parser.parse(agentString);
-			switch(agent.getOperatingSystem().getFamily()) {
-				case ANDROID:
-					result = OperatingSystem.ANDROID;
-					break;
-				case BLACKBERRY_OS:
-					result = OperatingSystem.BLACKBERRY;
-					break;
-				case IOS:
-					result = OperatingSystem.IOS;
-					break;
-				case LINUX:
-					result = OperatingSystem.LINUX;
-					break;
-				case MAC_OS:
-				case OS_X:
-					result = OperatingSystem.MACOS;
-					break;
-				case AIX:
-				case BSD:
-				case HPUX:
-				case IRIX:
-				case MINIX:
-				case SOLARIS:
-					result = OperatingSystem.UNIX;
-					break;
-				case WINDOWS:
-					result = OperatingSystem.WINDOWS;
-					break;
-				default:
-					result = OperatingSystem.OTHER;
-			}
-			osCache.put(agentString, result);
 		}
 		
 		return result;
