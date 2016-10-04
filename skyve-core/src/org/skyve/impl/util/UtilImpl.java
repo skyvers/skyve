b41bb9e1bce80c713c662f67e22c0e57b101f7b2
Mike Sands <mike.sands@bizhub.com.au>
2016-10-05 07:18:25 +1030

    Skyve 20161005.

--- skyve-core/src/org/skyve/impl/util/UtilImpl.java
+++ skyve-core/src/org/skyve/impl/util/UtilImpl.java
@@ -134,8 +134,8 @@ public class UtilImpl {
 	public static String PASSWORD_HASHING_ALGORITHM = "MD5"; 
 	
 	// For versioning javascript for web site
-	public static final String JAVASCRIPT_FILE_VERSION = "20161004";
-	public static final String SKYVE_VERSION = "20161004";
+	public static final String JAVASCRIPT_FILE_VERSION = "20161005";
+	public static final String SKYVE_VERSION = "20161005";
 	public static final String SMART_CLIENT_DIR = "isomorphic110";
 	
 	private static String absoluteBasePath;
