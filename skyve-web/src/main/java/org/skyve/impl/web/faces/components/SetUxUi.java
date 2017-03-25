31a8e5ff1996a49cc908ee8fd441f28040fcfb8f
Mike Sands <mike.sands@bizhub.com.au>
2017-03-25 22:55:03 +1030

    Add Skyve setUxUi tag.

---
+++ skyve-web/src/main/java/org/skyve/impl/web/faces/components/SetUxUi.java
@@ -0,0 +1,44 @@
+package org.skyve.impl.web.faces.components;
+
+import java.io.IOException;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.skyve.CORE;
+import org.skyve.impl.metadata.repository.router.Router;
+import org.skyve.impl.web.UserAgent;
+import org.skyve.impl.web.UserAgent.UserAgentType;
+import org.skyve.impl.web.faces.FacesUtil;
+import org.skyve.metadata.router.UxUi;
+import org.skyve.metadata.router.UxUiSelector;
+
+@FacesComponent(SetUxUi.COMPONENT_TYPE)
+public class SetUxUi extends UIComponentBase {
+	public static final String COMPONENT_TYPE = "org.skyve.impl.web.faces.components.SetUxUi";
+
+	@Override
+	public String getFamily() {
+		return "setUxUi";
+	}
+
+	
+	@Override
+	public void encodeBegin(FacesContext context) throws IOException {
+		// Set the UX/UI and user agent type
+		HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+		UserAgentType userAgentType = UserAgent.getType(request);
+		request.setAttribute(FacesUtil.USER_AGENT_TYPE_KEY, userAgentType);
+		Router router = CORE.getRepository().getRouter();
+		UxUi uxui;
+		try {
+			uxui = ((UxUiSelector) router.getUxuiSelector()).select(userAgentType, request);
+		}
+		catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		request.setAttribute(FacesUtil.UX_UI_KEY, uxui);
+	}
+}
