93f520fdc7928a9235b92cfc700da31f3ce5c499
Mike Sands <mike.sands@bizhub.com.au>
2017-09-19 07:47:55 +0930

    Ensure Content Garbage Collection continues to collect when an error occurs.

--- skyve-ext/src/org/skyve/impl/job/ContentGarbageCollectionJob.java
+++ skyve-ext/src/org/skyve/impl/job/ContentGarbageCollectionJob.java
@@ -3,6 +3,7 @@ package org.skyve.impl.job;
 import java.util.Date;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.logging.Level;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -38,50 +39,60 @@ public class ContentGarbageCollectionJob implements Job {
 			try {
 				try (ContentManager cm = EXT.newContentManager()) {
 					for (SearchResult result : cm.all()) {
-						if (UtilImpl.CONTENT_TRACE) UtilImpl.LOGGER.finest("ContentGarbageCollectionJob: FOUND customer=" + result.getCustomerName() + 
-																			" : module=" + result.getModuleName() + 
-																			" : document=" + result.getDocumentName() + 
-																			" : bizId=" + result.getBizId() + 
-																			" : attribute=" + result.getAttributeName() + 
-																			" : lastModified=" + result.getLastModified());
-						Date lastModified = result.getLastModified();
-						// only process this if its at least a day old.
-						// Besides cutting out busy work on a data set in flux, it'll make sure that anyones freshly uploaded
-						// content that hasn't been saved (not pointed to yet in the database) won't be removed.
-						if ((lastModified == null) || ((System.currentTimeMillis() - lastModified.getTime()) > 86400000)) {
-							Customer customer = r.getCustomer(result.getCustomerName());
-							Module module = customer.getModule(result.getModuleName());
-							Document document = module.getDocument(customer, result.getDocumentName());
-							
-							SQL query = null;
-							StringBuilder sql = new StringBuilder(128);
-							sql.append("select 1 from ").append(document.getPersistent().getPersistentIdentifier());
-							sql.append(" where ").append(Bean.DOCUMENT_ID).append(" = :").append(Bean.DOCUMENT_ID);
-							
-							// check if we have a record
-							String attributeName = result.getAttributeName();
-							if (attributeName != null) { // attachment
-								sql.append(" and ").append(attributeName).append(" = :").append(attributeName);
-	
-								query = p.newSQL(sql.toString());
-								query.putParameter(Bean.DOCUMENT_ID, result.getBizId(), false);
-								query.putParameter(attributeName, result.getContentId(), false);
+						try { // don't stop trying to detect removed content
+							if (UtilImpl.CONTENT_TRACE) UtilImpl.LOGGER.finest("ContentGarbageCollectionJob: FOUND customer=" + result.getCustomerName() + 
+																				" : module=" + result.getModuleName() + 
+																				" : document=" + result.getDocumentName() + 
+																				" : bizId=" + result.getBizId() + 
+																				" : attribute=" + result.getAttributeName() + 
+																				" : lastModified=" + result.getLastModified());
+							Date lastModified = result.getLastModified();
+							// only process this if its at least a day old.
+							// Besides cutting out busy work on a data set in flux, it'll make sure that anyones freshly uploaded
+							// content that hasn't been saved (not pointed to yet in the database) won't be removed.
+							if ((lastModified == null) || ((System.currentTimeMillis() - lastModified.getTime()) > 86400000)) {
+								Customer customer = r.getCustomer(result.getCustomerName());
+								Module module = customer.getModule(result.getModuleName());
+								Document document = module.getDocument(customer, result.getDocumentName());
+								
+								SQL query = null;
+								StringBuilder sql = new StringBuilder(128);
+								sql.append("select 1 from ").append(document.getPersistent().getPersistentIdentifier());
+								sql.append(" where ").append(Bean.DOCUMENT_ID).append(" = :").append(Bean.DOCUMENT_ID);
+								
+								// check if we have a record
+								String attributeName = result.getAttributeName();
+								if (attributeName != null) { // attachment
+									sql.append(" and ").append(attributeName).append(" = :").append(attributeName);
+		
+									query = p.newSQL(sql.toString());
+									query.putParameter(Bean.DOCUMENT_ID, result.getBizId(), false);
+									query.putParameter(attributeName, result.getContentId(), false);
+								}
+								else { // bean
+									query = p.newSQL(sql.toString());
+									query.putParameter(Bean.DOCUMENT_ID, result.getBizId(), false);
+								}
+								
+								if (UtilImpl.CONTENT_TRACE) UtilImpl.LOGGER.finest("ContentGarbageCollectionJob: TEST REMOVAL with " + sql.toString());
+								if (query.scalarResults(Integer.class).isEmpty()) {
+									orphanedContentIds.add(result.getContentId());
+								}
 							}
-							else { // bean
-								query = p.newSQL(sql.toString());
-								query.putParameter(Bean.DOCUMENT_ID, result.getBizId(), false);
-							}
-							
-							if (UtilImpl.CONTENT_TRACE) UtilImpl.LOGGER.finest("ContentGarbageCollectionJob: TEST REMOVAL with " + sql.toString());
-							if (query.scalarResults(Integer.class).isEmpty()) {
-								orphanedContentIds.add(result.getContentId());
-							}
+						}
+						catch (Exception e) {
+							Util.LOGGER.log(Level.WARNING, "ContentGarbageCollectionJob.execute() problem...", e);
 						}
 					}
 					
 					for (String contentId : orphanedContentIds) {
-						if (UtilImpl.CONTENT_TRACE) UtilImpl.LOGGER.info("ContentGarbageCollectionJob: Remove content with ID " + contentId);
-						cm.remove(contentId);
+						try { // don't stop trying to remove content
+							if (UtilImpl.CONTENT_TRACE) UtilImpl.LOGGER.info("ContentGarbageCollectionJob: Remove content with ID " + contentId);
+							cm.remove(contentId);
+						}
+						catch (Exception e) {
+							Util.LOGGER.log(Level.WARNING, "ContentGarbageCollectionJob.execute() problem...", e);
+						}
 					}
 				}
 			}
