a1596b5d92dba05d2956a9feceb10c2100f8fe47
Mike Sands <mike.sands@bizhub.com.au>
2018-02-19 12:30:31 +1030

    Add insert/put JSON endpoints to RestService.

--- skyve-web/src/main/java/org/skyve/impl/web/service/rest/RestService.java
+++ skyve-web/src/main/java/org/skyve/impl/web/service/rest/RestService.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -126,6 +127,42 @@ public class RestService {
 	    		Util.populateFully(bean);
 	    	}
 			result = JSON.marshall(CORE.getUser().getCustomer(), beans, null);
+		}
+		catch (Throwable t) {
+			t.printStackTrace();
+			AbstractRestFilter.error(p, response, t.getLocalizedMessage());
+		}
+		
+		return result;
+	}
+
+	@GET
+	@Path("/json/insert/{bean}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String insertJSONGet(@PathParam("bean") String json) {
+		return insertJSON(json);
+	}
+
+	@PUT
+	@Path("/json/insert")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String insertJSONPost(String json) {
+		return insertJSON(json);
+	}
+	
+	private String insertJSON(String json) {
+		String result = null;
+		
+		Persistence p = null;
+		try {
+			response.setContentType(MediaType.APPLICATION_JSON);
+			p = CORE.getPersistence();
+			User u = p.getUser();
+			
+			PersistentBean bean = (PersistentBean) JSON.unmarshall(u, json);
+			bean = p.save(bean);
+			result = JSON.marshall(u.getCustomer(), bean, null);
 		}
 		catch (Throwable t) {
 			t.printStackTrace();
