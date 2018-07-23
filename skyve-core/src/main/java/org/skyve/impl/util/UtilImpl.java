9b7d5afd57ceebdf4a7161541068a5ffac0b020c
Mike Sands <mike.sands@bizhub.com.au>
2018-07-23 22:51:03 +0930

    Skyve 20180723.

--- skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
+++ skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
@@ -152,8 +152,8 @@ public class UtilImpl {
 	public static String BOOTSTRAP_PASSWORD = null;
 	
 	// For versioning javascript/css etc for web site
-	public static final String WEB_RESOURCE_FILE_VERSION = "20180609-SNAPSHOT";
-	public static final String SKYVE_VERSION = "20180609-SNAPSHOT";
+	public static final String WEB_RESOURCE_FILE_VERSION = "20180723";
+	public static final String SKYVE_VERSION = "20180723";
 	public static final String SMART_CLIENT_DIR = "isomorphic110";
 
 	// for skyve script
