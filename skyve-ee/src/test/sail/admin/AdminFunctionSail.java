6a8474b6228d640d21ec84cbec3f1ffd6c8301e1
rbb555 <robert.brown@bizhub.com.au>
2018-10-17 22:22:01 +1100

    Tweak Sail tests for admin module as part of regression test

--- skyve-ee/src/test/sail/admin/AdminFunctionSail.java
+++ skyve-ee/src/test/sail/admin/AdminFunctionSail.java
@@ -7,25 +7,27 @@ public class AdminFunctionSail extends AdminSail {
 	public void test() throws Exception {
 		login("demo", "admin", "admin");
 		
-		trace("List for default query of [admin.User]");
-		get("?a=l&m=admin&q=User");
-		trace("Select on row 1 on list grid [admin.User] (s0)");
-		listGridSelect("s0", 1);
-		trace("Auto complete with search 'A' on lookup description [contact] (s3:s25)");
-		lookupDescription("s3:s25", "C");
-		lookupDescription("s3:s25", 0);
-
 		testMenuPassword();
 		testMenuUserDashboard();
 		testMenuContacts();
-		testMenuCommunications();
+		
+		//problem with dynamic domain
+		testMenuCommunications(); 
+		
 		testMenuSecurityAdminGroups();
 		testMenuSecurityAdminDataGroups();
+		
+		//fails on dynamic domain
 		testMenuDevOpsDataMaintenance();
+		
 		testMenuDevOpsDocumentCreator();
 		testMenuSnapshots();
+
 		testMenuSystemDashboard();
+		
+		//fails with error
 		testMenuDocumentNumbers();
+		
 		testMenuJobs();
 	}
 }
