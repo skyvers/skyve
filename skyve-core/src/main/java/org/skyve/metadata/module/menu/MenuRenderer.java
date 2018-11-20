a86acbc061103fa71bb8c90161f8e808966805b5
Mike Sands <mike.sands@bizhub.com.au>
2018-11-20 21:39:45 +1030

    Introduce MenuRenderer.

---
+++ skyve-core/src/main/java/org/skyve/metadata/module/menu/MenuRenderer.java
@@ -0,0 +1,300 @@
+package org.skyve.metadata.module.menu;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.skyve.CORE;
+import org.skyve.impl.metadata.module.menu.CalendarItem;
+import org.skyve.impl.metadata.module.menu.EditItem;
+import org.skyve.impl.metadata.module.menu.LinkItem;
+import org.skyve.impl.metadata.module.menu.ListItem;
+import org.skyve.impl.metadata.module.menu.MapItem;
+import org.skyve.impl.metadata.module.menu.TreeItem;
+import org.skyve.impl.metadata.user.UserImpl;
+import org.skyve.metadata.MetaDataException;
+import org.skyve.metadata.customer.Customer;
+import org.skyve.metadata.model.document.Document;
+import org.skyve.metadata.module.Module;
+import org.skyve.metadata.module.query.MetaDataQueryDefinition;
+
+public class MenuRenderer {
+	protected String uxui;
+	protected Locale locale;
+	protected String selectedModuleName;
+	
+	protected MenuRenderer(String uxui, Locale locale, String selectedModuleName) {
+		this.uxui = uxui;
+		this.locale = locale;
+		this.selectedModuleName = selectedModuleName;
+	}
+	
+	public void renderModuleMenu(@SuppressWarnings("unused") Menu menu,
+									@SuppressWarnings("unused") Module module,
+									@SuppressWarnings("unused") boolean open) {
+		// nothing to do
+	}
+	
+	public void renderMenuRoot(@SuppressWarnings("unused") Menu menu, @SuppressWarnings("unused") Module module) {
+		// nothing to do
+	}
+	
+	public void renderMenuGroup(@SuppressWarnings("unused") MenuGroup group) {
+		// nothing to do
+	}
+	
+	public void renderTreeItem(@SuppressWarnings("unused") TreeItem item,
+								@SuppressWarnings("unused") Module itemModule,
+								@SuppressWarnings("unused") Document itemDocument,
+								@SuppressWarnings("unused") String itemQueryName,
+								@SuppressWarnings("unused") String icon16,
+								@SuppressWarnings("unused") String iconStyleClass) {
+		// nothing to do
+	}
+	
+	public void renderListItem(@SuppressWarnings("unused") ListItem item,
+								@SuppressWarnings("unused") Module itemModule,
+								@SuppressWarnings("unused") Document itemDocument,
+								@SuppressWarnings("unused") String itemQueryName,
+								@SuppressWarnings("unused") String icon16,
+								@SuppressWarnings("unused") String iconStyleClass) {
+		// nothing to do
+	}
+	
+	public void renderCalendarItem(@SuppressWarnings("unused") CalendarItem item,
+									@SuppressWarnings("unused") Module itemModule,
+									@SuppressWarnings("unused") Document itemDocument,
+									@SuppressWarnings("unused") String itemQueryName,
+									@SuppressWarnings("unused") String icon16,
+									@SuppressWarnings("unused") String iconStyleClass) {
+		// nothing to do
+	}
+	
+	public void renderMapItem(@SuppressWarnings("unused") MapItem item,
+								@SuppressWarnings("unused") Module itemModule,
+								@SuppressWarnings("unused") Document itemDocument,
+								@SuppressWarnings("unused") String itemQueryName,
+								@SuppressWarnings("unused") String icon16,
+								@SuppressWarnings("unused") String iconStyleClass) {
+		// nothing to do
+	}
+	
+	public void renderEditItem(@SuppressWarnings("unused") EditItem item,
+								@SuppressWarnings("unused") Module itemModule,
+								@SuppressWarnings("unused") Document itemDocument,
+								@SuppressWarnings("unused") String icon16,
+								@SuppressWarnings("unused") String iconStyleClass) {
+		// nothing to do
+	}
+	
+	public void renderLinkItem(@SuppressWarnings("unused") LinkItem item,
+								@SuppressWarnings("unused") boolean relative) {
+		// nothing to do
+	}
+
+	public void renderedMenuGroup(@SuppressWarnings("unused") MenuGroup group) {
+		// nothing to do
+	}
+	
+	public void renderedMenuRoot(@SuppressWarnings("unused") Menu menu, @SuppressWarnings("unused") Module module) {
+		// nothing to do
+	}
+	
+	public void renderedModuleMenu(@SuppressWarnings("unused") Menu menu,
+									@SuppressWarnings("unused") Module module,
+									@SuppressWarnings("unused") boolean open) {
+		// nothing to do
+	}
+
+	public void render() {
+		UserImpl user = (UserImpl) CORE.getUser();
+		Customer customer = user.getCustomer();
+		
+		// determine if the first menu should be open - ie no default
+		Menu chosenMenu = (selectedModuleName == null) ? null : user.getModuleMenu(selectedModuleName);
+		final boolean setFirstModuleOpen = (chosenMenu == null) || chosenMenu.getItems().isEmpty();
+		final AtomicBoolean first = new AtomicBoolean(true);
+
+		// render each module menu
+		List<Module> modules = customer.getModules();
+		for (int i = 0, l = modules.size(); i < l; i++) {
+			Module module = modules.get(i);
+			String moduleName = module.getName();
+
+			Menu menu = user.getModuleMenu(moduleName);
+			if (menu.isApplicable(uxui)) {
+				boolean open = false;
+				if (setFirstModuleOpen) {
+					open = first.get();
+					first.set(false);
+				} 
+				else {
+					open = moduleName.equals(selectedModuleName);
+				}
+
+				renderModuleMenu(menu, module, open);
+				renderMenuRoot(menu, module);
+				renderMenuItems(customer, module, menu.getItems());
+				renderedMenuRoot(menu, module);
+				renderedModuleMenu(menu, module, open);
+			}
+		}
+	}
+
+	private void renderMenuItems(Customer customer,
+									Module module, 
+									List<MenuItem> items) {
+		for (MenuItem item : items) {
+			if (item.isApplicable(uxui)) {
+				if (item instanceof MenuGroup) {
+					MenuGroup group = (MenuGroup) item;
+					renderMenuGroup(group);
+					renderMenuItems(customer, module, group.getItems());
+					renderedMenuGroup(group);
+				}
+				else {
+					String icon16 = null;
+					String iconStyleClass = null;
+					Module itemModule = null;
+					String itemDocumentName = null;
+	                if (item instanceof TreeItem) {
+	                    TreeItem treeItem = (TreeItem) item;
+	                    itemDocumentName = treeItem.getDocumentName();
+						String itemQueryName = treeItem.getQueryName();
+						String modelName = treeItem.getModelName();
+						if (modelName != null) {
+							itemModule = customer.getModule(module.getDocument(customer, itemDocumentName).getOwningModuleName());
+						}
+						else {
+		                    MetaDataQueryDefinition query = deriveDocumentQuery(customer,
+													                                module,
+													                                item,
+													                                itemQueryName,
+													                                itemDocumentName);
+							itemDocumentName = query.getDocumentName();
+							itemQueryName = query.getName();
+							itemModule = query.getDocumentModule(customer);
+						}
+						Document itemDocument = itemModule.getDocument(customer, itemDocumentName);
+						icon16 = itemDocument.getIcon16x16RelativeFileName();
+						iconStyleClass = itemDocument.getIconStyleClass();
+	                    renderTreeItem(treeItem, itemModule, itemDocument, itemQueryName, icon16, iconStyleClass);
+	                }
+	                else if (item instanceof ListItem) {
+						ListItem listItem = (ListItem) item;
+						itemDocumentName = listItem.getDocumentName();
+						String itemQueryName = listItem.getQueryName();
+						String modelName = listItem.getModelName();
+						if (modelName != null) {
+							itemModule = customer.getModule(module.getDocument(customer, itemDocumentName).getOwningModuleName());
+						}
+						else {
+							MetaDataQueryDefinition query = deriveDocumentQuery(customer,
+													                                module,
+													                                item,
+													                                itemQueryName,
+													                                itemDocumentName);
+							itemDocumentName = query.getDocumentName();
+							itemQueryName = query.getName();
+							itemModule = query.getDocumentModule(customer);
+						}
+						Document itemDocument = itemModule.getDocument(customer, itemDocumentName);
+						icon16 = itemDocument.getIcon16x16RelativeFileName();
+						iconStyleClass = itemDocument.getIconStyleClass();
+						renderListItem(listItem, itemModule, itemDocument, itemQueryName, icon16, iconStyleClass);
+					}
+					else if (item instanceof CalendarItem) {
+	                    CalendarItem calendarItem = (CalendarItem) item;
+	                    itemDocumentName = calendarItem.getDocumentName();
+						MetaDataQueryDefinition query = deriveDocumentQuery(customer,
+												                                module,
+												                                item,
+												                                calendarItem.getQueryName(),
+												                                itemDocumentName);
+						itemDocumentName = query.getDocumentName();
+						String itemQueryName = query.getName();
+						itemModule = query.getDocumentModule(customer);
+						Document itemDocument = itemModule.getDocument(customer, itemDocumentName);
+						icon16 = itemDocument.getIcon16x16RelativeFileName();
+						iconStyleClass = itemDocument.getIconStyleClass();
+						renderCalendarItem(calendarItem, itemModule, itemDocument, itemQueryName, icon16, iconStyleClass);
+	                }
+	                else if (item instanceof MapItem) {
+	                    MapItem mapItem = (MapItem) item;
+	                    itemDocumentName = mapItem.getDocumentName();
+						String itemQueryName = mapItem.getQueryName();
+						String modelName = mapItem.getModelName();
+						if (modelName != null) {
+							itemModule = customer.getModule(module.getDocument(customer, itemDocumentName).getOwningModuleName());
+						}
+						else {
+							MetaDataQueryDefinition query = deriveDocumentQuery(customer,
+													                                module,
+													                                item,
+													                                itemQueryName,
+													                                itemDocumentName);
+							itemQueryName = query.getName();
+							itemModule = query.getDocumentModule(customer);
+						}
+						Document itemDocument = itemModule.getDocument(customer, itemDocumentName);
+						icon16 = itemDocument.getIcon16x16RelativeFileName();
+						iconStyleClass = itemDocument.getIconStyleClass();
+						renderMapItem(mapItem, itemModule, itemDocument, itemQueryName, icon16, iconStyleClass);
+	                }
+					else if (item instanceof EditItem) {
+						EditItem editItem = (EditItem) item;
+						itemDocumentName = editItem.getDocumentName();
+						itemModule = module;
+						Document itemDocument = itemModule.getDocument(customer, itemDocumentName);
+						icon16 = itemDocument.getIcon16x16RelativeFileName();
+						iconStyleClass = itemDocument.getIconStyleClass();
+						renderEditItem(editItem, itemModule, itemDocument, icon16, iconStyleClass);
+					}
+					else if (item instanceof LinkItem) {
+						LinkItem linkItem = (LinkItem) item;
+						String href = linkItem.getHref();
+						boolean relative = true;
+						try {
+							if (new URI(href).isAbsolute()) {
+								relative = false;
+							}
+						}
+						catch (@SuppressWarnings("unused") URISyntaxException e) {
+							// do nothing here if its not know to be absolute
+						}
+			        	
+						renderLinkItem(linkItem, relative);
+					}
+				}
+			}
+		}
+	}
+	
+	public static MetaDataQueryDefinition deriveDocumentQuery(Customer customer,
+																Module module,
+																MenuItem item,
+																String queryName,
+																String documentName) {
+        MetaDataQueryDefinition query = null;
+		if (queryName != null) {
+            query = module.getMetaDataQuery(queryName);
+            if ((query == null) || (query.getName() == null)) {
+                MetaDataException me = new MetaDataException("The target query " + queryName + " for menu action " +
+                                                                item.getName() + " is invalid in module " + module.getName());
+                throw me;
+            }
+        }
+        else {
+            query = module.getDocumentDefaultQuery(customer, documentName);
+            if ((query == null) || (query.getName() == null)) {
+                MetaDataException me = new MetaDataException("The target document " + documentName + " for menu action " +
+                                                                item.getName() + " has no default query in module " + module.getName());
+                throw me;
+            }
+        }
+
+        return query;
+	}
+}
