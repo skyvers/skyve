5967fa783e6a6dab086ee944482acdfe6b5a083a
rbb555 <robert.brown@bizhub.com.au>
2019-04-19 06:51:50 +0930

    Change group view to use listMembership for Role selection

--- skyve-ee/src/skyve/modules/admin/GroupRole/GroupRoleBizlet.java
+++ skyve-ee/src/skyve/modules/admin/GroupRole/GroupRoleBizlet.java
@@ -3,8 +3,11 @@ package modules.admin.GroupRole;
 import java.util.List;
 
 import org.skyve.CORE;
+import org.skyve.domain.Bean;
 import org.skyve.metadata.model.document.Bizlet;
+import org.skyve.web.WebContext;
 
+import modules.admin.Group.GroupBizlet;
 import modules.admin.User.UserBizlet;
 import modules.admin.domain.GroupRole;
 
@@ -20,4 +23,25 @@ public class GroupRoleBizlet extends Bizlet<GroupRole> {
 
 		return super.getVariantDomainValues(fieldName);
 	}
+
+	@Override
+	public GroupRole resolve(String bizId, Bean conversationBean, WebContext webContext) throws Exception {
+	
+		// Check if the role being resolved is part of the temporary collection for selection
+		// in the case of the listMembership selection, the bizId value being resolved
+		// will be the roleName
+		@SuppressWarnings("unchecked")
+		List<DomainValue> roles = (List<DomainValue>) CORE.getStash().get(GroupBizlet.AVAILABLE_ROLES);
+		for(DomainValue v: roles) {
+			if(v.getCode().equals(bizId)) {
+				GroupRole r = GroupRole.newInstance();
+				r.setRoleName(v.getCode());
+				return r;
+			}
+		}
+		
+		return super.resolve(bizId, conversationBean, webContext);
+	}
+	
+	
 }
