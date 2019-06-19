9d235b8ee16cfa42a6ed27956b44565cdeb16645
Mike Sands <mike.sands@bizhub.com.au>
2019-06-20 06:44:14 +0930

    Fix Tests

---
+++ skyve-ee/src/skyve/modules/admin/Configuration/ConfigurationFactory.java
@@ -0,0 +1,8 @@
+package modules.admin.Configuration;
+
+import org.skyve.util.test.SkyveFactory;
+
+@SkyveFactory(testDomain = false)
+public class ConfigurationFactory {
+	// nothing to see here
+}
