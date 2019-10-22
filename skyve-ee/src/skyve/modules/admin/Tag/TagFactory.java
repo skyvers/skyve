fef2f6ec7e3e1b1147bf4cd52cfa2f691186c8df
Ben Petito <benpetito@gmail.com>
2019-10-22 23:33:48 +1030

    Update AbstractH2Test to create an admin user at the start of each test
    - update generated admin tests to pass

--- skyve-ee/src/skyve/modules/admin/Tag/TagFactory.java
+++ skyve-ee/src/skyve/modules/admin/Tag/TagFactory.java
@@ -8,10 +8,11 @@ import org.skyve.util.test.SkyveFixture.FixtureType;
 import modules.admin.Tag.actions.BulkDocumentAction;
 import modules.admin.Tag.actions.PerformCombination;
 import modules.admin.Tag.actions.PrepareExplanation;
+import modules.admin.Tag.actions.TagAll;
 import modules.admin.domain.Contact;
 import modules.admin.domain.Tag;
 
-@SkyveFactory(excludedActions = { BulkDocumentAction.class, PerformCombination.class, PrepareExplanation.class })
+@SkyveFactory(excludedActions = { BulkDocumentAction.class, PerformCombination.class, PrepareExplanation.class, TagAll.class })
 public class TagFactory {
 
 	@SkyveFixture(types = FixtureType.crud)
