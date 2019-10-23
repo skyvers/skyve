c4cd66816b3ca9017f0b6d41258aef4c39bfc527
Mike Sands <mike.sands@bizhub.com.au>
2019-10-24 01:26:11 +1030

    Cater for document scoping in SingletonCachedBean.

--- skyve-core/src/main/java/org/skyve/metadata/model/document/SingletonCachedBizlet.java
+++ skyve-core/src/main/java/org/skyve/metadata/model/document/SingletonCachedBizlet.java
@@ -3,12 +3,16 @@ package org.skyve.metadata.model.document;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.skyve.CORE;
 import org.skyve.domain.Bean;
 import org.skyve.domain.PersistentBean;
+import org.skyve.metadata.user.DocumentPermissionScope;
+import org.skyve.metadata.user.User;
 import org.skyve.util.Util;
 
 /**
- * A Thread-safe cached singleton Bizlet implementation that memoises the super.newInstance() call.
+ * A Thread-safe cached singleton Bizlet implementation that memoises the super.newInstance() call
+ * if the permission is global or customer scoped.
  * evict() evicts the cache instance and postSave() calls evict().
  *
  * @param <T>
@@ -22,12 +26,28 @@ public abstract class SingletonCachedBizlet<T extends PersistentBean> extends Si
 	private static final ConcurrentMap<String, Bean> INSTANCES = new ConcurrentHashMap<>();
 	
 	/**
-	 * Gets a memoised version of the singleton instance.
+	 * Gets a memoised version of the singleton instance (if it is customer or global scoped).
 	 */
 	@Override
 	@SuppressWarnings("unchecked")
 	public T newInstance(T bean) throws Exception {
-		String key = bean.getBizModule() + bean.getBizDocument();
+		String bizModule = bean.getBizModule();
+		String bizDocument = bean.getBizDocument();
+		User u = CORE.getUser();
+		DocumentPermissionScope scope = u.getScope(bizModule, bizDocument);
+		String key = null;
+		if (DocumentPermissionScope.customer.equals(scope)) {
+			key = new StringBuilder(128).append(bizModule).append('.').append(bizDocument).append(u.getCustomerName()).append('.').toString();
+		}
+		else if (DocumentPermissionScope.global.equals(scope)) {
+			key = new StringBuilder(128).append(bizModule).append('.').append(bizDocument).toString();
+		}
+		
+		// not customer or global scoped, so no caching
+		if (key == null) {
+			return super.newInstance(bean);
+		}
+
 		Bean result = INSTANCES.get(key);
 		if (result == null) {
 			result = super.newInstance(bean);
@@ -42,15 +62,14 @@ public abstract class SingletonCachedBizlet<T extends PersistentBean> extends Si
 	 */
 	@Override
 	public void postSave(T bean) throws Exception {
-		evict(bean.getBizModule(), bean.getBizDocument());
+		evict(bean);
 	}
 
 	/**
 	 * Evicts the instance from the cache.
-	 * @param bizModule
-	 * @param bizDocument
+	 * @param bean
 	 */
-	public static void evict(String bizModule, String bizDocument) {
-		INSTANCES.remove(bizModule + bizDocument);
+	public synchronized void evict(T bean) {
+		INSTANCES.entrySet().removeIf(e -> e.getValue().getBizId().equals(bean.getBizId())); 
 	}
 }
