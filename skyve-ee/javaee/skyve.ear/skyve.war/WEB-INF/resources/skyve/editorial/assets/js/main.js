daaf9da15f45de719b71e6a6977f2f334f428473
Mike Sands <mike.sands@bizhub.com.au>
2019-11-05 07:41:32 +1030

    Remove "Toggle" from side link in editorial.

--- skyve-ee/javaee/skyve.ear/skyve.war/WEB-INF/resources/skyve/editorial/assets/js/main.js
+++ skyve-ee/javaee/skyve.ear/skyve.war/WEB-INF/resources/skyve/editorial/assets/js/main.js
@@ -89,7 +89,7 @@
 					.appendTo($head);
 
 		// Toggle.
-			$('<a href="#sidebar" class="toggle">Toggle</a>')
+			$('<a href="#sidebar" class="toggle"> </a>')
 				.appendTo($sidebar)
 				.on('click', function(event) {
 
