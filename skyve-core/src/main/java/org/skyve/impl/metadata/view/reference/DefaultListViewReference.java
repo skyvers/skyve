1a69ece4f6bb9fc18b0349e9a89e576856879116
Mike Sands <mike.sands@bizhub.com.au>
2020-03-18 08:06:25 +1030

    Remove binding from DefaultListViewReference.

--- skyve-core/src/main/java/org/skyve/impl/metadata/view/reference/DefaultListViewReference.java
+++ skyve-core/src/main/java/org/skyve/impl/metadata/view/reference/DefaultListViewReference.java
@@ -4,7 +4,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import org.skyve.impl.metadata.view.widget.bound.AbstractBound;
 import org.skyve.impl.util.UtilImpl;
 import org.skyve.impl.util.XMLMetaData;
 
@@ -15,7 +14,7 @@ import org.skyve.impl.util.XMLMetaData;
  */
 @XmlType(namespace = XMLMetaData.VIEW_NAMESPACE)
 @XmlRootElement(namespace = XMLMetaData.VIEW_NAMESPACE)
-public class DefaultListViewReference extends AbstractBound implements Reference {
+public class DefaultListViewReference implements Reference {
 	private static final long serialVersionUID = -1982657919843061491L;
 	
 	private String moduleName;
