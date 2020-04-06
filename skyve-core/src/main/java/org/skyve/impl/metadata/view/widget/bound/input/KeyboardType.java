da8ea68ad68358df2f7349f450d480dc55762287
Mike Sands <mike.sands@bizhub.com.au>
2020-04-07 06:47:46 +0930

    Do doco on KeyboardType.

--- skyve-core/src/main/java/org/skyve/impl/metadata/view/widget/bound/input/KeyboardType.java
+++ skyve-core/src/main/java/org/skyve/impl/metadata/view/widget/bound/input/KeyboardType.java
@@ -6,13 +6,16 @@ import org.skyve.impl.util.XMLMetaData;
 
 /**
  * Represents different keyboard modes on mobile devices.
+ * These are not automatically assigned by Skyve view generation mainly because the numeric/decimal 
+ * keyboards do not include a '-' button on their keyboard on some mobile devices (iOS) 
+ * and there is no way to know whether the input should accept negative numbers in Skyve.
  */
 @XmlType(namespace = XMLMetaData.VIEW_NAMESPACE)
 public enum KeyboardType {
-	numeric,
-	tel,
-	decimal,
-	email,
-	url,
-	search
+	numeric, // useful for positive whole numbers (as '-' is not always on the keyboard)
+	tel, // useful for telephone numbers
+	decimal, // usefu for positive decimal numbers (as '-' is not always on the keyboard)
+	email, // useful for entering emails
+	url, // useful for entering urls
+	search // normal keyboard but with 'search' key
 }
