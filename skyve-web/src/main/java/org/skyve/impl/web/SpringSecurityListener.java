cc3305fe73a435dedc4ba6d1fa95be243dcfa06f
Mike Sands <mike.sands@bizhub.com.au>
2020-05-11 20:17:20 +0930

    Reduce chances of page locking in the database during post login processing.

--- skyve-web/src/main/java/org/skyve/impl/web/SpringSecurityListener.java
+++ skyve-web/src/main/java/org/skyve/impl/web/SpringSecurityListener.java
@@ -2,6 +2,8 @@ package org.skyve.impl.web;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.skyve.EXT;
 import org.skyve.impl.persistence.hibernate.AbstractHibernatePersistence;
@@ -20,67 +22,54 @@ public class SpringSecurityListener {
 	@EventListener
 	@SuppressWarnings("static-method")
 	public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent evt) {
-		String bizCustomer = "";
 		String username = (String) evt.getAuthentication().getPrincipal();
-		if (username != null) {
-			int slashIndex = username.indexOf('/');
-			if (slashIndex > 0) {
-				bizCustomer = username.substring(0, slashIndex);
-				username = username.substring(slashIndex + 1);
-			}
-		}
 		UtilImpl.LOGGER.warning("Login Attempt failed for user " + username);
-		recordLoginFailure(bizCustomer, username);
+		recordLoginFailure(username);
 	}
 	
 	@EventListener
 	@SuppressWarnings("static-method")
 	public void onAuthenticationSuccess(AuthenticationSuccessEvent evt) {
-		String bizCustomer = "";
 		String username = ((User) evt.getAuthentication().getPrincipal()).getUsername();
-		if (username != null) {
-			int slashIndex = username.indexOf('/');
-			if (slashIndex > 0) {
-				bizCustomer = username.substring(0, slashIndex);
-				username = username.substring(slashIndex + 1);
-			}
-		}
 		UtilImpl.LOGGER.info("Login Attempt succeeded for user " + username);
-		resetLoginFailure(bizCustomer, username);
+		resetLoginFailure(username);
 	}
 	
-	private static void recordLoginFailure(String bizCustomer, String username) {
+	private static void recordLoginFailure(String username) {
 		SkyveDialect dialect = AbstractHibernatePersistence.getDialect(UtilImpl.DATA_STORE.getDialectClassName());
 		RDBMS rdbms = dialect.getRDBMS();
 		String sql = null;
 		if (RDBMS.h2.equals(rdbms)) {
-			sql = "update ADM_SecurityUser set authenticationFailures = ifNull(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizCustomer = ? and userName = ?";
+			sql = "update ADM_SecurityUser set authenticationFailures = ifNull(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizId = ?";
 		}
 		else if (RDBMS.mysql.equals(rdbms)) {
-			sql = "update ADM_SecurityUser set authenticationFailures = ifNull(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizCustomer = ? and userName = ?";
+			sql = "update ADM_SecurityUser set authenticationFailures = ifNull(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizId = ?";
 		}
 		else if (RDBMS.sqlserver.equals(rdbms)) {
-			sql = "update ADM_SecurityUser set authenticationFailures = coalesce(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizCustomer = ? and userName = ?";
+			sql = "update ADM_SecurityUser set authenticationFailures = coalesce(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizId = ?";
 		}
 		else if (RDBMS.postgresql.equals(rdbms)) {
-			sql = "update ADM_SecurityUser set authenticationFailures = coalesce(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizCustomer = ? and userName = ?";
+			sql = "update ADM_SecurityUser set authenticationFailures = coalesce(authenticationFailures, 0) + 1, lastAuthenticationFailure = ? where bizId = ?";
 		}
 		else {
 			UtilImpl.LOGGER.warning("Login Failure for " + username + " was not recorded because " + rdbms + " is not suported in SpringSecurityListener");
 			return;
 		}
 
+		// NB select and then update by bizId to ensure row lock across all databases.
 		try (Connection c = EXT.getDataStoreConnection()) {
-			try (PreparedStatement ps = c.prepareStatement(sql)) {
-				ps.setTimestamp(1, new java.sql.Timestamp(System.currentTimeMillis()));
-				ps.setString(2, bizCustomer);
-				ps.setString(3, username);
-				ps.executeUpdate();
-				c.commit();
-			}
-			catch (Exception e) {
-				c.rollback();
-				throw e;
+			String bizId = getBizId(username, c);
+			if (bizId != null) {
+				try (PreparedStatement ps = c.prepareStatement(sql)) {
+					ps.setTimestamp(1, new java.sql.Timestamp(System.currentTimeMillis()));
+					ps.setString(2, bizId);
+					ps.executeUpdate();
+					c.commit();
+				}
+				catch (Exception e) {
+					c.rollback();
+					throw e;
+				}
 			}
 		}
 		catch (Exception e) {
@@ -88,23 +77,51 @@ public class SpringSecurityListener {
 		}
 	}
 
-	private static void resetLoginFailure(String bizCustomer, String username) {
-		String sql = "update ADM_SecurityUser set authenticationFailures = 0, lastAuthenticationFailure = null where bizCustomer = ? and userName = ?";
+	private static void resetLoginFailure(String username) {
+		String sql = "update ADM_SecurityUser set authenticationFailures = 0, lastAuthenticationFailure = null where bizId = ?";
 
+		// NB select and then update by bizId to ensure row lock across all databases.
 		try (Connection c = EXT.getDataStoreConnection()) {
-			try (PreparedStatement ps = c.prepareStatement(sql)) {
-				ps.setString(1, bizCustomer);
-				ps.setString(2, username);
-				ps.executeUpdate();
-				c.commit();
-			}
-			catch (Exception e) {
-				c.rollback();
-				throw e;
+			String bizId = getBizId(username, c);
+			if (bizId != null) {
+				try (PreparedStatement ps = c.prepareStatement(sql)) {
+					ps.setString(1, bizId);
+					ps.executeUpdate();
+					c.commit();
+				}
+				catch (Exception e) {
+					c.rollback();
+					throw e;
+				}
 			}
 		}
 		catch (Exception e) {
 			throw new MetaDataException(e);
 		}
+	}
+	
+	private static String getBizId(String principalName, Connection c) throws SQLException {
+		String result = null;
+		
+		String bizCustomer = null;
+		String username = null;
+		if (principalName != null) {
+			int slashIndex = principalName.indexOf('/');
+			if (slashIndex > 0) {
+				bizCustomer = principalName.substring(0, slashIndex);
+				username = principalName.substring(slashIndex + 1);
+			}
+		}
+		try (PreparedStatement ps = c.prepareStatement("select bizId from ADM_SecurityUser where bizCustomer = ? and userName = ?")) {
+			ps.setString(1, bizCustomer);
+			ps.setString(2, username);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					result = rs.getString(1);
+				}
+			}
+		}
+		
+		return result;
 	}
 }
