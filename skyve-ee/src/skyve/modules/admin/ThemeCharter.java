64d11b8657aa2be86262ada3b919fe7e148d4698
rbb555 <robert.brown@bizhub.com.au>
2020-10-13 15:52:00 +1030

    Provide multicolour and non-sql dataset options for ThemeCharter

--- skyve-ee/src/skyve/modules/admin/ThemeCharter.java
+++ skyve-ee/src/skyve/modules/admin/ThemeCharter.java
@@ -27,6 +27,7 @@ import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.chart.title.TextTitle;
+import org.jfree.data.general.PieDataset;
 import org.jfree.data.jdbc.JDBCCategoryDataset;
 import org.jfree.data.jdbc.JDBCPieDataset;
 import org.skyve.EXT;
@@ -38,18 +39,121 @@ public class ThemeCharter {
 	private static final String THEME_FONT_NAME = "Arial";
 	private static final int THEME_NORMAL_FONT_SIZE = 12;
 	private static final int THEME_HEADER_FONT_SIZE = 14;
-	private static final Font THEME_LEGEND_FONT = new Font(THEME_FONT_NAME, Font.PLAIN, THEME_NORMAL_FONT_SIZE);
-	private static final Font THEME_TITLE_FONT = new Font(THEME_FONT_NAME, Font.BOLD, THEME_NORMAL_FONT_SIZE);
-	private static final Font THEME_RANGE_FONT = new Font(THEME_FONT_NAME, Font.PLAIN, THEME_HEADER_FONT_SIZE);
-	private static final Font THEME_DOMAIN_FONT = new Font(THEME_FONT_NAME, Font.PLAIN, THEME_NORMAL_FONT_SIZE);
-	
+
 	private static final String NO_DATA_AVAILABLE = "No data available";
 	private static final Color WHITE = new Color(255, 255, 255);
 	private static final Color BLACK = new Color(0, 0, 0);
-	
-	
+    
 	private String sql;
-	private Color themeColour;
+	
+	private String themeFontName;
+	private int themeLegendFontSize;
+	private int themeTitleFontSize;
+	private int themeRangeFontSize;
+	private int themeDomainFontSize;
+	private SectionColouriser.Colouriser colouriserTheme;
+	private Color themeBaseColour;
+	private String[] colourPalette = {
+			"#9898FF",
+			"#EBC663",
+			"#BD74F7",
+			"#DEB1A0",
+			"#82BDCE",
+			"#65F8DF",
+			"#A2C4A4",
+			"#B76CBE",
+			"#D6D879",
+			"#EAA3FE",
+			"#E3E3AD",
+			"#EF7880",
+			"#A8E5FF",
+			"#86A9D8",
+			"#94E76E",
+			"#B779CC",
+			"#DAAFA5",
+			"#FFA6D2",
+			"#E3E3AD"
+	};
+	
+	public Color getThemeBaseColour() {
+		return themeBaseColour;
+	}
+
+	public void setThemeBaseColour(Color themeBaseColour) {
+		this.themeBaseColour = themeBaseColour;
+	}
+	
+	public String[] getColourPalette() {
+		return colourPalette;
+	}
+
+	public void setColourPalette(String[] colourPalette) {
+		this.colourPalette = colourPalette;
+	}
+
+	public SectionColouriser.Colouriser getColouriserTheme() {
+		return colouriserTheme;
+	}
+
+	public void setColouriserTheme(SectionColouriser.Colouriser colouriserTheme) {
+		this.colouriserTheme = colouriserTheme;
+	}
+
+	public int getThemeLegendFontSize() {
+		return themeLegendFontSize;
+	}
+
+	public void setThemeLegendFontSize(int themeLegendFontSize) {
+		this.themeLegendFontSize = themeLegendFontSize;
+	}
+
+	public int getThemeTitleFontSize() {
+		return themeTitleFontSize;
+	}
+
+	public void setThemeTitleFontSize(int themeTitleFontSize) {
+		this.themeTitleFontSize = themeTitleFontSize;
+	}
+
+	public int getThemeRangeFontSize() {
+		return themeRangeFontSize;
+	}
+
+	public void setThemeRangeFontSize(int themeRangeFontSize) {
+		this.themeRangeFontSize = themeRangeFontSize;
+	}
+
+	public int getThemeDomainFontSize() {
+		return themeDomainFontSize;
+	}
+
+	public void setThemeDomainFontSize(int themeDomainFontSize) {
+		this.themeDomainFontSize = themeDomainFontSize;
+	}
+
+	public Font getThemeLegendFont() {
+		return new Font(themeFontName, Font.PLAIN, themeLegendFontSize);
+	}
+
+	public Font getThemeTitleFont() {
+		return new Font(themeFontName, Font.BOLD, themeTitleFontSize);
+	}
+
+	public Font getThemeRangeFont() {
+		return new Font(themeFontName, Font.PLAIN, themeRangeFontSize);
+	}
+
+	public Font getThemeDomainFont() {
+		return new Font(themeFontName, Font.PLAIN, themeDomainFontSize);
+	}
+
+	public String getThemeFontName() {
+		return themeFontName;
+	}
+
+	public void setThemeFontName(String themeFontName) {
+		this.themeFontName = themeFontName;
+	}
 
 	public String getSql() {
 		return sql;
@@ -59,39 +163,86 @@ public class ThemeCharter {
 		this.sql = sql;
 	}
 
-	public Color getThemeColour() {
-		return themeColour;
-	}
-
-	public void setThemeColour(Color themeColour) {
-		this.themeColour = themeColour;
-	}
-
 	public enum ChartAspect {
 		FLAT, THREE_D
 	}
 
 	public ThemeCharter(Color themeColour) {
-		this.themeColour = themeColour;
+		this.themeBaseColour = themeColour;
+		themeFontName = THEME_FONT_NAME;
+		themeLegendFontSize = THEME_HEADER_FONT_SIZE;
+		themeTitleFontSize = THEME_HEADER_FONT_SIZE;
+		themeRangeFontSize = THEME_NORMAL_FONT_SIZE;
+		themeDomainFontSize = THEME_NORMAL_FONT_SIZE;
 	}
 
 	public ThemeCharter() {
-		this.themeColour = new Color(70, 130, 180);
+		this.themeBaseColour = new Color(70, 130, 180);
+		themeFontName = THEME_FONT_NAME;
+		themeLegendFontSize = THEME_HEADER_FONT_SIZE;
+		themeTitleFontSize = THEME_HEADER_FONT_SIZE;
+		themeRangeFontSize = THEME_NORMAL_FONT_SIZE;
+		themeDomainFontSize = THEME_NORMAL_FONT_SIZE;
 	}
 
-	private static class SectionColouriser {
+	public static class SectionColouriser {
+		public enum Colouriser {
+			SINGLE_COLOUR, MULTI_COLOUR
+		}
+		
 		private int redDiff;
 		private int greenDiff;
 		private int blueDiff;
-
+		private Colouriser colouriser = Colouriser.SINGLE_COLOUR;
+		private int currentColourIndex = 0;
+		
 		private Color current;
+		private String[] palette;
+		
+		public Colouriser getColouriser() {
+			return colouriser;
+		}
+
+		public void setColour(Colouriser colouriser) {
+			this.colouriser = colouriser;
+		}
 
 		public Color getCurrent() {
 			return current;
 		}
 
-		public SectionColouriser(Color baseColour, int columnCount) {
+		public void setCurrent(Color current) {
+			this.current = current;
+		}
+
+		public int getRedDiff() {
+			return redDiff;
+		}
+
+		public void setRedDiff(int redDiff) {
+			this.redDiff = redDiff;
+		}
+
+		public int getGreenDiff() {
+			return greenDiff;
+		}
+
+		public void setGreenDiff(int greenDiff) {
+			this.greenDiff = greenDiff;
+		}
+
+		public int getBlueDiff() {
+			return blueDiff;
+		}
+
+		public void setBlueDiff(int blueDiff) {
+			this.blueDiff = blueDiff;
+		}
+		
+		public SectionColouriser(Color baseColour, int columnCount, SectionColouriser.Colouriser colouriser, String[] palette) {
 			this.current = baseColour;
+			this.colouriser = colouriser;
+			this.palette = palette;
 			if (columnCount != 0) {
 				this.redDiff = (baseColour.getRed() / 2) / columnCount;
 				this.greenDiff = (baseColour.getGreen() / 2) / columnCount;
@@ -100,12 +251,18 @@ public class ThemeCharter {
 		}
 
 		public void nextColour() {
-			this.current = new Color(current.getRed() - redDiff, current.getGreen() - greenDiff, current.getBlue() - blueDiff);
+			if(colouriser==Colouriser.MULTI_COLOUR) {
+				this.current = Color.decode(palette[currentColourIndex++]);
+			} else {
+				this.current = new Color(current.getRed() - redDiff, current.getGreen() - greenDiff,
+					current.getBlue() - blueDiff);
+			}
 		}
-
 	}
 
-	public BufferedImage getBarChartImage(String domainTitle, String rangeTitle, Integer labelColumn, PlotOrientation orientation, int width, int height, ChartAspect aspect, boolean showLegend) throws Exception {
+	public BufferedImage getBarChartImage(String domainTitle, String rangeTitle, Integer labelColumn,
+			PlotOrientation orientation, int width, int height, ChartAspect aspect, boolean showLegend)
+			throws Exception {
 		Connection connection = null;
 		try {
 			connection = EXT.getDataStoreConnection();
@@ -113,9 +270,11 @@ public class ThemeCharter {
 
 			JFreeChart chart;
 			if (ChartAspect.THREE_D.equals(aspect)) {
-				chart = ChartFactory.createBarChart3D(EMPTY_STRING, domainTitle, rangeTitle, data, orientation, true, false, false);
+				chart = ChartFactory.createBarChart3D(EMPTY_STRING, domainTitle, rangeTitle, data, orientation, true,
+						false, false);
 			} else {
-				chart = ChartFactory.createBarChart(EMPTY_STRING, domainTitle, rangeTitle, data, orientation, true, false, false);
+				chart = ChartFactory.createBarChart(EMPTY_STRING, domainTitle, rangeTitle, data, orientation, true,
+						false, false);
 			}
 			chart.setBackgroundImageAlpha(0.8F);
 			chart.getPlot().setBackgroundAlpha(0.2F);
@@ -137,7 +296,7 @@ public class ThemeCharter {
 			plot.setOutlineVisible(false);
 
 			// generate generic series colours
-			SectionColouriser colouriser = new ThemeCharter.SectionColouriser(this.themeColour, plot.getDataset().getColumnCount());
+			SectionColouriser colouriser = new SectionColouriser(this.themeBaseColour, plot.getDataset().getColumnCount(), getColouriserTheme(), getColourPalette());
 
 			// set series renderers
 			CategoryItemLabelGenerator generator = null;
@@ -156,12 +315,12 @@ public class ThemeCharter {
 			renderer.setItemLabelAnchorOffset(0);
 
 			TextTitle title = chart.getTitle();
-			title.setFont(THEME_TITLE_FONT);
+			title.setFont(getThemeTitleFont());
 
-			plot.getDomainAxis().setLabelFont(THEME_DOMAIN_FONT);
-			plot.getRangeAxis().setLabelFont(THEME_RANGE_FONT);
+			plot.getDomainAxis().setLabelFont(getThemeDomainFont());
+			plot.getRangeAxis().setLabelFont(getThemeRangeFont());
 
-			chart.getLegend().setItemFont(THEME_LEGEND_FONT);
+			chart.getLegend().setItemFont(getThemeLegendFont());
 			chart.getLegend().setVisible(showLegend);
 
 			return chart.createBufferedImage(width, height);
@@ -181,12 +340,14 @@ public class ThemeCharter {
 		return null;
 	}
 
-	public BufferedImage getLineChartImage(String domainTitle, String rangeTitle, Integer labelColumn, PlotOrientation orientation, int width, int height, boolean showLegend) throws Exception {
+	public BufferedImage getLineChartImage(String domainTitle, String rangeTitle, Integer labelColumn,
+			PlotOrientation orientation, int width, int height, boolean showLegend) throws Exception {
 		Connection connection = null;
 		try {
 			connection = EXT.getDataStoreConnection();
 			JDBCCategoryDataset data = new JDBCCategoryDataset(connection, this.sql);
-			JFreeChart chart = ChartFactory.createLineChart(EMPTY_STRING, domainTitle, rangeTitle, data, orientation, true, false, false);
+			JFreeChart chart = ChartFactory.createLineChart(EMPTY_STRING, domainTitle, rangeTitle, data, orientation,
+					true, false, false);
 
 			chart.setBackgroundImageAlpha(0.8F);
 			chart.getPlot().setBackgroundAlpha(0.2F);
@@ -202,7 +363,7 @@ public class ThemeCharter {
 			plot.setNoDataMessage(NO_DATA_AVAILABLE);
 			plot.setOutlineVisible(false);
 
-			SectionColouriser colouriser = new ThemeCharter.SectionColouriser(themeColour, plot.getDataset().getColumnCount());
+			SectionColouriser colouriser = new SectionColouriser(this.themeBaseColour, plot.getDataset().getColumnCount(), getColouriserTheme(), getColourPalette());
 
 			CategoryItemLabelGenerator generator = null;
 			if (labelColumn != null) {
@@ -220,12 +381,12 @@ public class ThemeCharter {
 			renderer.setItemLabelAnchorOffset(0);
 
 			TextTitle title = chart.getTitle();
-			title.setFont(THEME_TITLE_FONT);
+			title.setFont(getThemeTitleFont());
 
-			plot.getDomainAxis().setLabelFont(THEME_DOMAIN_FONT);
-			plot.getRangeAxis().setLabelFont(THEME_RANGE_FONT);
+			plot.getDomainAxis().setLabelFont(getThemeDomainFont());
+			plot.getRangeAxis().setLabelFont(getThemeRangeFont());
 
-			chart.getLegend().setItemFont(THEME_LEGEND_FONT);
+			chart.getLegend().setItemFont(getThemeLegendFont());
 			chart.getLegend().setVisible(showLegend);
 
 			return chart.createBufferedImage(width, height);
@@ -245,12 +406,14 @@ public class ThemeCharter {
 		return null;
 	}
 
-	public BufferedImage getAreaChartImage(String domainTitle, String rangeTitle, Integer labelColumn, PlotOrientation orientation, int width, int height, boolean showLegend) throws Exception {
+	public BufferedImage getAreaChartImage(String domainTitle, String rangeTitle, Integer labelColumn,
+			PlotOrientation orientation, int width, int height, boolean showLegend) throws Exception {
 		Connection connection = null;
 		try {
 			connection = EXT.getDataStoreConnection();
 			JDBCCategoryDataset data = new JDBCCategoryDataset(connection, this.sql);
-			JFreeChart chart = ChartFactory.createAreaChart(EMPTY_STRING, domainTitle, rangeTitle, data, orientation, true, false, false);
+			JFreeChart chart = ChartFactory.createAreaChart(EMPTY_STRING, domainTitle, rangeTitle, data, orientation,
+					true, false, false);
 
 			chart.setBackgroundImageAlpha(0.8F);
 			chart.getPlot().setBackgroundAlpha(0.2F);
@@ -266,7 +429,7 @@ public class ThemeCharter {
 			plot.setNoDataMessage(NO_DATA_AVAILABLE);
 			plot.setOutlineVisible(false);
 
-			SectionColouriser colouriser = new ThemeCharter.SectionColouriser(themeColour, plot.getDataset().getColumnCount());
+			SectionColouriser colouriser = new SectionColouriser(this.themeBaseColour, plot.getDataset().getColumnCount(), getColouriserTheme(), getColourPalette());
 
 			CategoryItemLabelGenerator generator = null;
 			if (labelColumn != null) {
@@ -284,12 +447,12 @@ public class ThemeCharter {
 			renderer.setItemLabelAnchorOffset(0);
 
 			TextTitle title = chart.getTitle();
-			title.setFont(THEME_TITLE_FONT);
+			title.setFont(getThemeTitleFont());
 
-			plot.getDomainAxis().setLabelFont(THEME_DOMAIN_FONT);
-			plot.getRangeAxis().setLabelFont(THEME_RANGE_FONT);
+			plot.getDomainAxis().setLabelFont(getThemeDomainFont());
+			plot.getRangeAxis().setLabelFont(getThemeRangeFont());
 
-			chart.getLegend().setItemFont(THEME_LEGEND_FONT);
+			chart.getLegend().setItemFont(getThemeLegendFont());
 			chart.getLegend().setVisible(showLegend);
 
 			return chart.createBufferedImage(width, height);
@@ -309,55 +472,61 @@ public class ThemeCharter {
 		return null;
 	}
 
-	public BufferedImage getPieChartImage(String title, Integer labelColumn, int width, int height, ChartAspect aspect, boolean showLegend) throws Exception {
+	public BufferedImage getPieChartImage(String title, Integer labelColumn, int width, int height, ChartAspect aspect,
+			boolean showLegend, PieDataset data) throws Exception {
+
+		JFreeChart chart;
+		if (ChartAspect.THREE_D.equals(aspect)) {
+			chart = ChartFactory.createPieChart3D(title, data, true, false, false);
+		} else {
+			chart = ChartFactory.createPieChart(title, data, true, false, false);
+		}
+		chart.setBackgroundImageAlpha(0.0F);
+		chart.getPlot().setBackgroundAlpha(0.0F);
+		chart.setBackgroundPaint(null);
+
+		PiePlot plot = (PiePlot) chart.getPlot();
+		plot.setBackgroundAlpha(0.0F);
+		plot.setNoDataMessage(NO_DATA_AVAILABLE);
+
+		if (labelColumn != null) {
+			PieSectionLabelGenerator generator = new StandardPieSectionLabelGenerator(labelReference(labelColumn));
+			plot.setLabelGenerator(generator); // null means no labels
+		}
+
+		plot.setStartAngle(135);
+		plot.setOutlineVisible(false);
+
+		SectionColouriser colouriser = new SectionColouriser(this.themeBaseColour, plot.getDataset().getItemCount(), getColouriserTheme(), getColourPalette());
+
+		for (int seriesIndex = 0; seriesIndex < plot.getDataset().getItemCount(); seriesIndex++) {
+			plot.setSectionPaint(plot.getDataset().getKey(seriesIndex), colouriser.getCurrent());
+			colouriser.nextColour();
+		}
+
+		TextTitle textTitle = chart.getTitle();
+		textTitle.setFont(getThemeTitleFont());
+
+		plot.setSectionOutlinesVisible(true);
+		plot.setBaseSectionOutlinePaint(new Color(0xFFFFFF));
+		plot.setBaseSectionOutlineStroke(new BasicStroke(2F));
+
+		if (!showLegend) {
+			chart.removeLegend();
+		} else {
+			chart.getLegend().setItemFont(getThemeLegendFont());
+		}
+
+		return chart.createBufferedImage(width, height);
+	}
+
+	public BufferedImage getPieChartImage(String title, Integer labelColumn, int width, int height, ChartAspect aspect,
+			boolean showLegend) throws Exception {
 		Connection connection = null;
 		try {
-
 			connection = EXT.getDataStoreConnection();
 			JDBCPieDataset data = new JDBCPieDataset(connection, this.sql);
-			JFreeChart chart;
-			if (ChartAspect.THREE_D.equals(aspect)) {
-				chart = ChartFactory.createPieChart3D(title, data, true, false, false);
-			} else {
-				chart = ChartFactory.createPieChart(title, data, true, false, false);
-			}
-			chart.setBackgroundImageAlpha(0.0F);
-			chart.getPlot().setBackgroundAlpha(0.0F);
-			chart.setBackgroundPaint(null);
-
-			PiePlot plot = (PiePlot) chart.getPlot();
-			plot.setBackgroundAlpha(0.0F);
-			plot.setNoDataMessage(NO_DATA_AVAILABLE);
-
-			if (labelColumn != null) {
-				PieSectionLabelGenerator generator = new StandardPieSectionLabelGenerator(labelReference(labelColumn));
-				plot.setLabelGenerator(generator); // null means no labels
-			}
-
-			plot.setStartAngle(135);
-			plot.setOutlineVisible(false);
-
-			SectionColouriser colouriser = new ThemeCharter.SectionColouriser(themeColour, plot.getDataset().getItemCount());
-
-			for (int seriesIndex = 0; seriesIndex < plot.getDataset().getItemCount(); seriesIndex++) {
-				plot.setSectionPaint(plot.getDataset().getKey(seriesIndex), colouriser.getCurrent());
-				colouriser.nextColour();
-			}
-			
-			TextTitle textTitle = chart.getTitle();
-			textTitle.setFont(THEME_TITLE_FONT);
-
-			plot.setSectionOutlinesVisible(true);
-			plot.setBaseSectionOutlinePaint(new Color(0xFFFFFF));
-			plot.setBaseSectionOutlineStroke(new BasicStroke(2F));
-
-			if (!showLegend) {
-				chart.removeLegend();
-			} else {
-				chart.getLegend().setItemFont(THEME_LEGEND_FONT);				
-			}
-
-			return chart.createBufferedImage(width, height);
+			return getPieChartImage(title, labelColumn, width, height, aspect, showLegend, data);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -373,16 +542,17 @@ public class ThemeCharter {
 
 		return null;
 	}
-
+	
 	private static String labelReference(Integer columnIndex) {
 		return "{" + columnIndex.toString() + "}";
 	}
-	
+
 	/**
-	 * A Fabulator is an ordered coloured list of string-bigint combinations, typically to represent comparative totals
+	 * A Fabulator is an ordered coloured list of string-bigint combinations,
+	 * typically to represent comparative totals
 	 * 
 	 * List<Object[]> objects is the tupleResults() returned from SQL similar to
-	 * 	"select name, count(*) from a group by name"
+	 * "select name, count(*) from a group by name"
 	 * 
 	 * @param width
 	 * @param height
@@ -398,7 +568,7 @@ public class ThemeCharter {
 		graphics.setColor(backCol);
 		graphics.fillRect(0, 0, width, height);
 
-		Font font = THEME_TITLE_FONT;
+		Font font = getThemeTitleFont();
 		Canvas c = new Canvas();
 		FontMetrics fm = c.getFontMetrics(font);
 		graphics.setFont(font);
@@ -425,7 +595,7 @@ public class ThemeCharter {
 			topNumber = 1;
 		}
 
-		SectionColouriser colouriser = new SectionColouriser(this.themeColour, rows);
+		SectionColouriser colouriser = new SectionColouriser(this.themeBaseColour, rows, getColouriserTheme(), getColourPalette());
 		int counter = 0;
 
 		for (Object[] o : objects) {
