2227ea4e5bcce66b16e69913026ec83820614d22
Ben Petito <benpetito@gmail.com>
2020-10-28 21:50:26 +1030

    Update Skyve logo and favicons to use new logo

--- skyve-web/src/main/java/org/skyve/impl/web/faces/beans/Desktop.java
+++ skyve-web/src/main/java/org/skyve/impl/web/faces/beans/Desktop.java
@@ -178,7 +178,7 @@ public class Desktop extends Harness {
     	result.append("\"><tr height=\"46px\"><td width=\"1%\">{icon}</td>");
     	result.append("<td><div class=\"titleBar\">{title}</div></td>");
     	result.append("<td width=\"10%\" align=\"right\">");
-    	result.append("<img src=\"images/skyve_inv.png\" alt=\"Skyve\"/></td>");
+		result.append("<img src=\"images/skyve-thick-grey.png\" style=\"max-height: 28px; height: auto;\" alt=\"Skyve\"/></td>");
     	result.append("<td width=\"1%\" align=\"right\"><div class=\"skyveDocumentLink\">{link}</div></td>");
 		result.append("<td width=\"1%\" align=\"right\"><a href=\"javascript:setUxUi();\" class=\"dhtmlPageButton\" title=\"Switch Mode\"><i class=\"fa fa-2x fa-share-square-o\"></i></a></td>");
 		result.append("<td width=\"1%\" align=\"right\"><a href=\"javascript:isc.BizUtil.popupSearch();\" class=\"dhtmlPageButton\" title=\"Search\"><i class=\"fa fa-2x fa-search\"></i></a></td>");
