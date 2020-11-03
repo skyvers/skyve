f2e21f5affb1ef6b7d31774145b51d8ea1e45682
Mike Sands <mike.sands@bizhub.com.au>
2020-11-04 08:02:06 +1030

    Remove AdminFaces component builders.

--- skyve-web/src/main/java/org/skyve/impl/web/faces/pipeline/component/AdminFacesComponentBuilderChain.java
+++
@@ -1,7 +0,0 @@
-package org.skyve.impl.web.faces.pipeline.component;
-
-public class AdminFacesComponentBuilderChain extends ComponentBuilderChain {
-	public AdminFacesComponentBuilderChain() {
-		super(new DeviceResponsiveComponentBuilder(), new PaginatedListGridBuilder(), new AdminFacesComponentBuilder());
-	}
-}
