042160abd0a5e36262fcd882522699c5196fe5cb
Mike Sands <mike.sands@bizhub.com.au>
2021-01-28 01:39:42 +1030

    Fix content plugin.

--- skyve-ext/src/main/java/org/skyve/addin/DefaultAddInManager.java
+++ skyve-ext/src/main/java/org/skyve/addin/DefaultAddInManager.java
@@ -1,18 +1,9 @@
 package org.skyve.addin;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.pf4j.ClassLoadingStrategy;
-import org.pf4j.CompoundPluginLoader;
-import org.pf4j.DefaultPluginLoader;
 import org.pf4j.DefaultPluginManager;
-import org.pf4j.DevelopmentPluginLoader;
-import org.pf4j.JarPluginLoader;
-import org.pf4j.PluginClassLoader;
-import org.pf4j.PluginDescriptor;
-import org.pf4j.PluginLoader;
 import org.pf4j.PluginManager;
 import org.pf4j.PluginWrapper;
 import org.skyve.impl.util.UtilImpl;
@@ -31,44 +22,7 @@ public class DefaultAddInManager implements AddInManager {
 	}
 	
 	public void start() {
-		plugInManager = new DefaultPluginManager(Paths.get(UtilImpl.CONTENT_DIRECTORY, "addins")) {
-//			@Override
-//			public RuntimeMode getRuntimeMode() {
-//				return RuntimeMode.DEVELOPMENT;
-//			}
-
-			@Override
-			public ClassLoader getPluginClassLoader(String pluginId) {
-				// TODO Auto-generated method stub
-				return super.getPluginClassLoader(pluginId);
-			}
-			
-			@Override
-			protected PluginLoader createPluginLoader() {
-		        return new CompoundPluginLoader()
-		                .add(new DevelopmentPluginLoader(this) {
-		                	@Override
-		                	protected PluginClassLoader createPluginClassLoader(Path pluginPath, PluginDescriptor pluginDescriptor) {
-		                        return new PluginClassLoader(pluginManager, pluginDescriptor, getClass().getClassLoader(), ClassLoadingStrategy.ADP);
-		                	}
-		                }, this::isDevelopment)
-		                .add(new JarPluginLoader(this) {
-		                    @Override
-		                    public ClassLoader loadPlugin(Path pluginPath, PluginDescriptor pluginDescriptor) {
-		                        PluginClassLoader pluginClassLoader = new PluginClassLoader(pluginManager, pluginDescriptor, getClass().getClassLoader(), ClassLoadingStrategy.ADP);
-		                        pluginClassLoader.addFile(pluginPath.toFile());
-
-		                        return pluginClassLoader;
-		                    }
-		                }, this::isNotDevelopment)
-		                .add(new DefaultPluginLoader(this) {
-		                	@Override
-		                	protected PluginClassLoader createPluginClassLoader(Path pluginPath, PluginDescriptor pluginDescriptor) {
-		                        return new PluginClassLoader(pluginManager, pluginDescriptor, getClass().getClassLoader(), ClassLoadingStrategy.ADP);
-		                	}
-		                }, this::isNotDevelopment);
-			}
-		};
+		plugInManager = new DefaultPluginManager(Paths.get(UtilImpl.CONTENT_DIRECTORY, "addins"));
 		plugInManager.loadPlugins();
 		plugInManager.startPlugins();
 		
