85deccc487c076892f6a1c71b64b4a683e3295ab
Mike Sands <mike.sands@bizhub.com.au>
2021-02-11 16:42:36 +1030

    Skyve 6.0.3

--- skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
+++ skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
@@ -59,8 +59,8 @@ public class UtilImpl {
 	public static Map<String, Object> OVERRIDE_CONFIGURATION;
 
 	// For versioning javascript/css etc for web site
-	public static final String WEB_RESOURCE_FILE_VERSION = "33";
-	public static final String SKYVE_VERSION = "6.0.3-SNAPSHOT";
+	public static final String WEB_RESOURCE_FILE_VERSION = "34";
+	public static final String SKYVE_VERSION = "6.0.3";
 	public static final String SMART_CLIENT_DIR = "isomorphic120";
 
 	public static boolean XML_TRACE = false;
