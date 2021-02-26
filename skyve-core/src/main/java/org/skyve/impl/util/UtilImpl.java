74a918b9f313ed61978043bdf07f64574ff93cec
Mike Sands <mike.sands@bizhub.com.au>
2021-02-27 00:15:47 +1030

    Move to Skyve 7.0.3-SNAPSHOT

--- skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
+++ skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
@@ -58,8 +58,8 @@ public class UtilImpl {
 	public static Map<String, Object> OVERRIDE_CONFIGURATION;
 
 	// For versioning javascript/css etc for web site
-	public static final String WEB_RESOURCE_FILE_VERSION = "35";
-	public static final String SKYVE_VERSION = "7.0.2";
+	public static final String WEB_RESOURCE_FILE_VERSION = "36";
+	public static final String SKYVE_VERSION = "7.0.3-SNAPSHOT";
 	public static final String SMART_CLIENT_DIR = "isomorphic120";
 
 	public static boolean XML_TRACE = false;
