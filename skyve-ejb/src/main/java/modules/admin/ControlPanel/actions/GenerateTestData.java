d9f4ead98d8b24771531bad96d98e6fc3c54213e
Ben Petito <benpetito@gmail.com>
2021-04-01 23:54:45 +1030

    Update ControlPanel so Generate Test Data module name is not required
    - update GenerateTestData validation to use i18n for display names in validation messages

--- skyve-ejb/src/main/java/modules/admin/ControlPanel/actions/GenerateTestData.java
+++ skyve-ejb/src/main/java/modules/admin/ControlPanel/actions/GenerateTestData.java
@@ -10,6 +10,8 @@ import org.skyve.metadata.controller.ServerSideActionResult;
 import org.skyve.metadata.customer.Customer;
 import org.skyve.metadata.module.JobMetaData;
 import org.skyve.metadata.module.Module;
+import org.skyve.util.BeanValidator;
+import org.skyve.util.Util;
 import org.skyve.web.WebContext;
 
 import modules.admin.ControlPanel.ControlPanelExtension;
@@ -39,7 +41,8 @@ public class GenerateTestData implements ServerSideAction<ControlPanelExtension>
 		ValidationException ve = new ValidationException();
 
 		if (bean.getTestModuleName() == null) {
-			ve.getMessages().add(new Message(ControlPanel.testModuleNamePropertyName, "Module Name is required."));
+			ve.getMessages().add(new Message(ControlPanel.testModuleNamePropertyName,
+					Util.i18n(BeanValidator.VALIDATION_REQUIRED_KEY, Util.i18n("admin.controlPanel.testModuleName.displayName"))));
 		}
 
 		if (bean.getTestDocumentNames().size() == 0) {
@@ -47,13 +50,18 @@ public class GenerateTestData implements ServerSideAction<ControlPanelExtension>
 		}
 
 		if (bean.getTestNumberToGenerate() == null) {
-			ve.getMessages().add(new Message(ControlPanel.testNumberToGeneratePropertyName, "Number to Generate is required."));
+			ve.getMessages().add(new Message(ControlPanel.testNumberToGeneratePropertyName,
+					Util.i18n(BeanValidator.VALIDATION_REQUIRED_KEY,
+							Util.i18n("admin.controlPanel.testNumberToGenerate.displayName"))));
 		}
 
 		if (bean.getTestNumberToGenerate() == null || bean.getTestNumberToGenerate().intValue() < 1) {
-			ve.getMessages().add(new Message(ControlPanel.testNumberToGeneratePropertyName, "Number to Generate must be greater than 0."));
+			ve.getMessages().add(new Message(ControlPanel.testNumberToGeneratePropertyName,
+					Util.i18n("admin.controlPanel.testNumberToGenerate.displayName") + " must be greater than 0."));
+
 		} else if (bean.getTestNumberToGenerate().intValue() > 10000) {
-			ve.getMessages().add(new Message(ControlPanel.testNumberToGeneratePropertyName, "Number to Generate must be less than 10,000."));
+			ve.getMessages().add(new Message(ControlPanel.testNumberToGeneratePropertyName,
+					Util.i18n("admin.controlPanel.testNumberToGenerate.displayName") + " must be less than 10,000."));
 		}
 		
 		if(Boolean.TRUE.equals(bean.getTestTagGeneratedData()) && bean.getTestTagName()==null) {
