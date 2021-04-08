ee5f88112f99e9d5d1e4ba9b3cf7ae61bb854f0b
Mike Sands <mike.sands@bizhub.com.au>
2021-04-08 10:57:13 +0930

    Remove post method from ReportServlet.

--- skyve-web/src/main/java/org/skyve/impl/web/ReportServlet.java
+++ skyve-web/src/main/java/org/skyve/impl/web/ReportServlet.java
@@ -62,12 +62,6 @@ public class ReportServlet extends HttpServlet {
 	public static final String EXPORT_PATH = "/export";
 
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
 		AbstractPersistence persistence = AbstractPersistence.get();
