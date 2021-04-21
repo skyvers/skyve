f74febedaedf20ea1b5b2ccd3ec19c210d2838d9
Mike Sands <mike.sands@bizhub.com.au>
2021-04-22 09:11:55 +0930

    Enable multiple instances of <s:csrfForm/> easily.

--- skyve-web/src/main/java/org/skyve/impl/web/faces/components/CSRFForm.java
+++ skyve-web/src/main/java/org/skyve/impl/web/faces/components/CSRFForm.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import javax.el.ELContext;
 import javax.el.ExpressionFactory;
+import javax.faces.FacesException;
 import javax.faces.application.Application;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponent;
 import javax.faces.component.html.HtmlForm;
-import javax.faces.component.html.HtmlInputHidden;
+import javax.faces.component.html.HtmlOutputText;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.component.autoupdate.AutoUpdateListener;
+import org.skyve.impl.web.faces.FacesUtil;
+import org.skyve.impl.web.faces.beans.FacesView;
+import org.skyve.util.Util;
 
 @FacesComponent(CSRFForm.COMPONENT_TYPE)
 public class CSRFForm extends HtmlForm {
@@ -23,28 +27,41 @@ public class CSRFForm extends HtmlForm {
 		// Default to not be a naming container so that csrfToken goes down with that parameter name.
 		setPrependId(false);
 	}
-	
+
 	@Override
 	public void encodeBegin(FacesContext context) throws IOException {
 		List<UIComponent> children = getChildren();
-		if (children.isEmpty() || (! "csrfToken".equals(children.get(0).getId()))) {
+		if (children.isEmpty() || (! (children.get(0) instanceof HtmlOutputText))) {
 			FacesContext fc = FacesContext.getCurrentInstance();
 			Application a = fc.getApplication();
 			ExpressionFactory ef = a.getExpressionFactory();
 			ELContext elc = fc.getELContext();
 	
-	    	final String managedBeanName = (String) getAttributes().get("managedBean");
-	
+	    	final String managedBeanName = Util.processStringValue((String) getAttributes().get("managedBean"));
+	    	if (managedBeanName == null) {
+	    		throw new FacesException("managedBean attribute is required");
+	    	}
+	    	
+	    	FacesView<?> managedBean = null;
+			// Do nothing if this is being executed through SAIL
+	    	if (FacesContext.getCurrentInstance() != null) {
+				managedBean = FacesUtil.getManagedBean(managedBeanName);
+			}
+
 			// Add hidden csrfToken
-			// <h:inputHidden id="csrfToken" value="#{skyve.csrfToken}">
+			// <h:outputText id="#{skyve.nextId()}" style="display:none" escape="false" value="&lt;input type=&quot;hidden&quot; name=&quot;csrfToken&quot; value=&quot;#{fspView.csrfToken}&quot;/&gt;">
 			// 		<p:autoUpdate />
-			// </h:inputHidden>
-			HtmlInputHidden hidden = (HtmlInputHidden) a.createComponent(HtmlInputHidden.COMPONENT_TYPE);
-			hidden.setId("csrfToken");
-			hidden.setValueExpression("value", ef.createValueExpression(elc, "#{" + managedBeanName + ".csrfToken}", String.class));
-	        AutoUpdateListener.subscribe(hidden);
+			// </h:outputText>
+	    	HtmlOutputText text = (HtmlOutputText) a.createComponent(HtmlOutputText.COMPONENT_TYPE);
+	    	if (managedBean != null) {
+	    		text.setId(managedBean.nextId());
+	    	}
+	    	text.setStyle("display:none");
+	    	text.setEscape(false);
+	    	text.setValueExpression("value", ef.createValueExpression(elc, "<input type=\"hidden\" name=\"csrfToken\" value=\"#{" + managedBeanName + ".csrfToken}\"/>", String.class));
+	        AutoUpdateListener.subscribe(text);
 	
-			children.add(0, hidden);
+			children.add(0, text);
 		}
 		super.encodeBegin(context);
 	}
