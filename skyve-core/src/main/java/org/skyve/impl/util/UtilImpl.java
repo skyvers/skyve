84a6ca68e0c18ef18d9abd0e17655e332a4ee11f
Mike Sands <mike.sands@bizhub.com.au>
2021-11-11 11:14:31 +1030

    Move to Skyve 8.1.0-SNAPSHOT

--- skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
+++ skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
@@ -62,7 +62,7 @@ public class UtilImpl {
 
 	// For versioning javascript/css etc for web site
 	public static final String WEB_RESOURCE_FILE_VERSION = "42";
-	public static final String SKYVE_VERSION = "8.0.0";
+	public static final String SKYVE_VERSION = "8.1.0-SNAPSHOT";
 	public static final String SMART_CLIENT_DIR = "isomorphic120";
 
 	public static boolean XML_TRACE = false;
