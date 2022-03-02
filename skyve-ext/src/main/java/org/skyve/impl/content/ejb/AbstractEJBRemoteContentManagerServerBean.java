beba651b385506fc4a7599b6af1fa269f5292ced
Mike Sands <mike.sands@bizhub.com.au>
2022-03-02 22:40:00 +1030

    Add logging to EJB remote ContentManager classes.

--- skyve-ext/src/main/java/org/skyve/impl/content/ejb/AbstractEJBRemoteContentManagerServerBean.java
+++ skyve-ext/src/main/java/org/skyve/impl/content/ejb/AbstractEJBRemoteContentManagerServerBean.java
@@ -5,6 +5,7 @@ import org.skyve.content.AttachmentContent;
 import org.skyve.content.BeanContent;
 import org.skyve.content.ContentManager;
 import org.skyve.content.SearchResults;
+import org.skyve.util.Util;
 
 /**
  * Extend this to make a stateless session bean in the skyve server instance.
@@ -24,6 +25,7 @@ import org.skyve.content.SearchResults;
 public abstract class AbstractEJBRemoteContentManagerServerBean implements EJBRemoteContentManagerServer {
 	@Override
 	public void put(BeanContent content) throws Exception {
+		Util.LOGGER.info("Remote call to EJBRemoteContentManagerServer.put() received for " + content.getBizId());
 		try (ContentManager cm = EXT.newContentManager()) {
 			cm.put(content);
 		}
@@ -31,6 +33,7 @@ public abstract class AbstractEJBRemoteContentManagerServerBean implements EJBRe
 	
 	@Override
 	public String put(AttachmentContent content, boolean index) throws Exception {
+		Util.LOGGER.info("Remote call to EJBRemoteContentManagerServer.put() received for " + content.getBizId() + " attribute " + content.getAttributeName());
 		try (ContentManager cm = EXT.newContentManager()) {
 			cm.put(content, index);
 			return content.getContentId();
@@ -39,6 +42,7 @@ public abstract class AbstractEJBRemoteContentManagerServerBean implements EJBRe
 
 	@Override
 	public AttachmentContent getAttachment(String contentId) throws Exception {
+		Util.LOGGER.info("Remote call to EJBRemoteContentManagerServer.getAttachment() received for " + contentId);
 		try (ContentManager cm = EXT.newContentManager()) {
 			return cm.getAttachment(contentId);
 		}
@@ -46,6 +50,7 @@ public abstract class AbstractEJBRemoteContentManagerServerBean implements EJBRe
 
 	@Override
 	public void removeBean(String bizId) throws Exception {
+		Util.LOGGER.info("Remote call to EJBRemoteContentManagerServer.removeBean() received for " + bizId);
 		try (ContentManager cm = EXT.newContentManager()) {
 			cm.removeBean(bizId);
 		}
@@ -53,6 +58,7 @@ public abstract class AbstractEJBRemoteContentManagerServerBean implements EJBRe
 
 	@Override
 	public void removeAttachment(String contentId) throws Exception {
+		Util.LOGGER.info("Remote call to EJBRemoteContentManagerServer.removeAttachment() received for " + contentId);
 		try (ContentManager cm = EXT.newContentManager()) {
 			cm.removeAttachment(contentId);
 		}
@@ -60,6 +66,7 @@ public abstract class AbstractEJBRemoteContentManagerServerBean implements EJBRe
 	
 	@Override
 	public SearchResults google(String search, int maxResults) throws Exception {
+		Util.LOGGER.info("Remote call to EJBRemoteContentManagerServer.google() received for '" + search + '\'');
 		try (ContentManager cm = EXT.newContentManager()) {
 			return cm.google(search, maxResults);
 		}
