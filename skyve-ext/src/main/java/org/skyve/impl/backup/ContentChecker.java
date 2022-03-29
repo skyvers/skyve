21702c5b6d278c01ddc014e539b4c3ac21d3825d
Mike Sands <mike.sands@bizhub.com.au>
2022-03-29 15:06:34 +1030

    Check content meta data is correct in ContentChecker, not just that the content is present.

--- skyve-ext/src/main/java/org/skyve/impl/backup/ContentChecker.java
+++ skyve-ext/src/main/java/org/skyve/impl/backup/ContentChecker.java
@@ -13,86 +13,143 @@ import org.skyve.content.AttachmentContent;
 import org.skyve.content.ContentManager;
 import org.skyve.impl.content.AbstractContentManager;
 import org.skyve.impl.util.UtilImpl;
+import org.skyve.metadata.customer.Customer;
+import org.skyve.metadata.model.Attribute;
 import org.skyve.metadata.model.Attribute.AttributeType;
-import org.skyve.util.Util;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.skyve.metadata.model.Extends;
+import org.skyve.metadata.model.Persistent;
+import org.skyve.metadata.model.document.Document;
+import org.skyve.metadata.module.Module;
 
 public class ContentChecker {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ContentChecker.class);
-
-    @SuppressWarnings("static-method")
+	@SuppressWarnings("static-method")
 	public void checkContent() throws Exception {
-        String customerName = CORE.getUser().getCustomerName();
+		Customer customer = CORE.getCustomer();
+		String customerName = customer.getName();
 
-        try (Connection connection = EXT.getDataStoreConnection()) {
-            connection.setAutoCommit(false);
+		try (Connection connection = EXT.getDataStoreConnection()) {
+			connection.setAutoCommit(false);
 
-            try (ContentManager cm = EXT.newContentManager()) {
-            	int missingContentCount = 0;
-            	
-            	for (Table table : BackupUtil.getTables()) {
-                	if (! hasContent(table)) {
-                		continue;
-                	}
-                	
-                    StringBuilder sql = new StringBuilder(128);
-                    try (Statement statement = connection.createStatement()) {
-                        sql.append("select * from ").append(table.name);
-                        BackupUtil.secureSQL(sql, table, customerName);
-                        statement.execute(sql.toString());
-                        try (ResultSet resultSet = statement.getResultSet()) {
-                            LOGGER.info("Checking content for " + table.name);
+			try (ContentManager cm = EXT.newContentManager()) {
+				int missingContentCount = 0;
+				int erroneousContentCount = 0;
 
-                            while (resultSet.next()) {
-                                for (String name : table.fields.keySet()) {
-                                    AttributeType attributeType = table.fields.get(name);
+				for (Table table : BackupUtil.getTables()) {
+					if (! hasContent(table)) {
+						continue;
+					}
 
-                                    if (AttributeType.content.equals(attributeType) ||
-                                    		AttributeType.image.equals(attributeType)) {
-                                        String stringValue = resultSet.getString(name);
-                                        if (! resultSet.wasNull()) {
-                                            AttachmentContent content;
-                                            try {
-                                                content = cm.getAttachment(stringValue);
-                                                if (content == null) {
-                                                    LOGGER.error("Detected missing content {} for field name {} for table {}", stringValue, name, table.name);
+					StringBuilder sql = new StringBuilder(128);
+					try (Statement statement = connection.createStatement()) {
+						sql.append("select * from ").append(table.name);
+						BackupUtil.secureSQL(sql, table, customerName);
+						statement.execute(sql.toString());
+						try (ResultSet resultSet = statement.getResultSet()) {
+							UtilImpl.LOGGER.info("Checking content for " + table.name);
 
-                                                    // Construct what would be the file path.
-                                                    final File contentDirectory = Paths.get(UtilImpl.CONTENT_DIRECTORY, ContentManager.FILE_STORE_NAME).toFile();
-                                                    final StringBuilder contentAbsolutePath = new StringBuilder(contentDirectory.getAbsolutePath() + File.separator);
-                                                    AbstractContentManager.appendBalancedFolderPathFromContentId(stringValue, contentAbsolutePath, true);
-                                                    final File contentFile = Paths.get(contentAbsolutePath.toString(), stringValue).toFile();
+							while (resultSet.next()) {
+								for (String name : table.fields.keySet()) {
+									AttributeType attributeType = table.fields.get(name);
 
-                                                    if (contentFile.exists()) {
-                                                        LOGGER.error("Found matching file for missing content {}.", contentFile.getAbsolutePath());
-                                                    }
-                                                    missingContentCount++;
-                                                }
-                                            } catch (Exception e) {
-                                                LOGGER.error("Error checking content {} for field name {} for table {}", stringValue, name, table.name, e);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } catch (SQLException e) {
-                        Util.LOGGER.severe(sql.toString());
-                        throw e;
-                    }
-                }
-                connection.commit();
-                LOGGER.info("MISSING CONTENT COUNT = " + missingContentCount);
-            }
-        }
-    }
+									if (AttributeType.content.equals(attributeType) || AttributeType.image.equals(attributeType)) {
+										String stringValue = resultSet.getString(name);
+										if (! resultSet.wasNull()) {
+											AttachmentContent content;
+											try {
+												content = cm.getAttachment(stringValue);
+												if (content == null) {
+													UtilImpl.LOGGER.severe("Detected missing content " + stringValue + " for field name " + name + " for table " + table.name);
 
-    private static boolean hasContent(Table table) {
+													// Construct what would be the file path.
+													final File contentDirectory = Paths.get(UtilImpl.CONTENT_DIRECTORY, ContentManager.FILE_STORE_NAME).toFile();
+													final StringBuilder contentAbsolutePath = new StringBuilder(contentDirectory.getAbsolutePath() + File.separator);
+													AbstractContentManager.appendBalancedFolderPathFromContentId(stringValue, contentAbsolutePath, true);
+													final File contentFile = Paths.get(contentAbsolutePath.toString(), stringValue).toFile();
+
+													if (contentFile.exists()) {
+														UtilImpl.LOGGER.severe("Found matching file for missing content " + contentFile.getAbsolutePath());
+													}
+													missingContentCount++;
+												}
+												else {
+													String attributeName = content.getAttributeName();
+													if (! name.equals(attributeName)) {
+														UtilImpl.LOGGER.severe("Detected error in content " + stringValue + " for field name " + name + " for table " + table.name + ": Content Attribute Name " + attributeName + " does not match content field name " + name);
+														erroneousContentCount++;
+													}
+													else {
+														String bizModule = content.getBizModule();
+														String bizDocument = content.getBizDocument();
+														try {
+															Module m = customer.getModule(bizModule);
+															Document d = m.getDocument(customer, bizDocument);
+															Persistent p = d.getPersistent();
+															if (p == null) {
+																UtilImpl.LOGGER.severe("Detected error in content " + stringValue + " for field name " + name + " for table " + table.name + ": Content Document " + bizModule + "." + bizDocument + " is not persistent");
+																erroneousContentCount++;
+															}
+															else if (! table.name.equals(p.getPersistentIdentifier())) {
+																UtilImpl.LOGGER.severe("Detected error in content " + stringValue + " for field name " + name + " for table " + table.name + ": Content Document " + bizModule + "." + bizDocument + " has a persistent identifier of " + p.getPersistentIdentifier());
+																erroneousContentCount++;
+															}
+															else {
+																// Find the attribute in the document hierarchy
+																Attribute a = d.getAttribute(attributeName);
+																Document base = d;
+																Extends extension = d.getExtends();
+																while ((a == null) && (extension != null)) {
+																	Module basemod = customer.getModule(base.getOwningModuleName());
+																	base = basemod.getDocument(customer, extension.getDocumentName());
+																	a = base.getAttribute(attributeName);
+																	extension = base.getExtends();
+																}
+
+																if (a == null) {
+																	UtilImpl.LOGGER.severe("Detected error in content " + stringValue + " for field name " + name + " for table " + table.name + ": Content Attribute Name " + attributeName + " does not exist for document " + bizModule + "." + bizDocument);
+																	erroneousContentCount++;
+																}
+																else {
+																	AttributeType type = a.getAttributeType();
+																	if (type != attributeType) {
+																		UtilImpl.LOGGER.severe("Detected error in content " + stringValue + " for field name " + name + " for table " + table.name + ": Content Attribute Name " + name + " is not a(n) " + attributeType + " for document " + bizModule + "." + bizDocument);
+																		erroneousContentCount++;
+																	}
+																}
+															}
+														}
+														catch (@SuppressWarnings("unused") Exception e) {
+															UtilImpl.LOGGER.severe("Detected error in content " + stringValue + " for field name " + name + " for table " + table.name + ": Content Document " + bizModule + "." + bizDocument + " does not exist for customer " + customerName);
+															erroneousContentCount++;
+														}
+													}
+												}
+											}
+											catch (Exception e) {
+												UtilImpl.LOGGER.severe("Error checking content " + stringValue + " for field name " + name + " for table " + table.name);
+												e.printStackTrace();
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					catch (SQLException e) {
+						UtilImpl.LOGGER.severe(sql.toString());
+						throw e;
+					}
+				}
+				connection.commit();
+				UtilImpl.LOGGER.info("MISSING CONTENT COUNT = " + missingContentCount);
+				UtilImpl.LOGGER.info("ERRONEOUS CONTENT COUNT = " + erroneousContentCount);
+			}
+		}
+	}
+
+	private static boolean hasContent(Table table) {
 		for (String name : table.fields.keySet()) {
 			AttributeType attributeType = table.fields.get(name);
-			if (AttributeType.content.equals(attributeType) ||
-					AttributeType.image.equals(attributeType)) {
+			if (AttributeType.content.equals(attributeType) || AttributeType.image.equals(attributeType)) {
 				return true;
 			}
 		}
