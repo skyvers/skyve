916b4c4198da9a1b724c6ca2b801965698936770
Mike Sands <mike.sands@bizhub.com.au>
2022-05-10 17:11:50 +0930

    Add different domain types to kitchen sink.

--- skyve-ejb/src/generated/java/modules/kitchensink/domain/ListAttributes.java
+++ skyve-ejb/src/generated/java/modules/kitchensink/domain/ListAttributes.java
@@ -10,29 +10,37 @@ import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.skyve.CORE;
-import org.skyve.domain.Bean;
-import org.skyve.domain.ChildBean;
 import org.skyve.domain.messages.DomainException;
 import org.skyve.domain.types.DateOnly;
+import org.skyve.domain.types.DateTime;
+import org.skyve.domain.types.Decimal10;
+import org.skyve.domain.types.Decimal2;
 import org.skyve.domain.types.Decimal5;
 import org.skyve.domain.types.Enumeration;
-import org.skyve.impl.domain.AbstractTransientBean;
+import org.skyve.domain.types.TimeOnly;
+import org.skyve.domain.types.Timestamp;
+import org.skyve.impl.domain.AbstractPersistentBean;
 import org.skyve.impl.domain.types.jaxb.DateOnlyMapper;
+import org.skyve.impl.domain.types.jaxb.DateTimeMapper;
+import org.skyve.impl.domain.types.jaxb.Decimal10Mapper;
+import org.skyve.impl.domain.types.jaxb.Decimal2Mapper;
 import org.skyve.impl.domain.types.jaxb.Decimal5Mapper;
+import org.skyve.impl.domain.types.jaxb.TimeOnlyMapper;
+import org.skyve.impl.domain.types.jaxb.TimestampMapper;
 import org.skyve.metadata.model.document.Bizlet.DomainValue;
 import org.skyve.util.Binder;
 import org.skyve.util.ExpressionEvaluator;
 import org.skyve.util.Util;
 
 /**
- * Inline Grid
+ * List Attributes
  * 
  * @depend - - - ConstantEnum
- * @stereotype "transient child"
+ * @stereotype "persistent"
  */
 @XmlType
 @XmlRootElement
-public class InlineGrid extends AbstractTransientBean implements ChildBean<KitchenSink> {
+public class ListAttributes extends AbstractPersistentBean {
 	/**
 	 * For Serialization
 	 * @hidden
@@ -43,7 +51,7 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 	public static final String MODULE_NAME = "kitchensink";
 
 	/** @hidden */
-	public static final String DOCUMENT_NAME = "InlineGrid";
+	public static final String DOCUMENT_NAME = "ListAttributes";
 
 	/** @hidden */
 	public static final String constantEnumPropertyName = "constantEnum";
@@ -67,7 +75,40 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 	public static final String datePropertyName = "date";
 
 	/** @hidden */
+	public static final String dateTimePropertyName = "dateTime";
+
+	/** @hidden */
+	public static final String decimal10PropertyName = "decimal10";
+
+	/** @hidden */
+	public static final String decimal2PropertyName = "decimal2";
+
+	/** @hidden */
 	public static final String decimal5PropertyName = "decimal5";
+
+	/** @hidden */
+	public static final String idPropertyName = "id";
+
+	/** @hidden */
+	public static final String normalIntegerPropertyName = "normalInteger";
+
+	/** @hidden */
+	public static final String longIntegerPropertyName = "longInteger";
+
+	/** @hidden */
+	public static final String markupPropertyName = "markup";
+
+	/** @hidden */
+	public static final String memoPropertyName = "memo";
+
+	/** @hidden */
+	public static final String textPropertyName = "text";
+
+	/** @hidden */
+	public static final String timePropertyName = "time";
+
+	/** @hidden */
+	public static final String timestampPropertyName = "timestamp";
 
 	/**
 	 * ConstantEnum
@@ -183,27 +224,78 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 	private DateOnly date = (DateOnly) ExpressionEvaluator.evaluate("{el:newDateOnly()}", this);
 
 	/**
+	 * Date Time
+	 **/
+	private DateTime dateTime = (DateTime) ExpressionEvaluator.evaluate("{el:newDateTime()}", this);
+
+	/**
+	 * Decimal 10
+	 **/
+	private Decimal10 decimal10 = (Decimal10) ExpressionEvaluator.evaluate("{el:Decimal10.ZERO}", this);
+
+	/**
+	 * Decimal 2
+	 **/
+	private Decimal2 decimal2 = (Decimal2) ExpressionEvaluator.evaluate("{el:Decimal2.ZERO}", this);
+
+	/**
 	 * Decimal 5
 	 **/
 	private Decimal5 decimal5 = (Decimal5) ExpressionEvaluator.evaluate("{el:Decimal5.ZERO}", this);
 
-	private KitchenSink parent;
+	/**
+	 * Id
+	 **/
+	private String id;
 
-	private Integer bizOrdinal;
+	/**
+	 * Integer
+	 **/
+	private Integer normalInteger;
+
+	/**
+	 * Long Integer
+	 **/
+	private Long longInteger;
+
+	/**
+	 * Markup
+	 **/
+	private String markup;
+
+	/**
+	 * Memo
+	 **/
+	private String memo = Binder.formatMessage("Test {el:newGeometry('POINT(0 0)')}", this);
+
+	/**
+	 * Text
+	 **/
+	private String text;
+
+	/**
+	 * Time
+	 **/
+	private TimeOnly time;
+
+	/**
+	 * Timestamp
+	 **/
+	private Timestamp timestamp;
 
 	@Override
 	@XmlTransient
 	public String getBizModule() {
-		return InlineGrid.MODULE_NAME;
+		return ListAttributes.MODULE_NAME;
 	}
 
 	@Override
 	@XmlTransient
 	public String getBizDocument() {
-		return InlineGrid.DOCUMENT_NAME;
+		return ListAttributes.DOCUMENT_NAME;
 	}
 
-	public static InlineGrid newInstance() {
+	public static ListAttributes newInstance() {
 		try {
 			return CORE.getUser().getCustomer().getModule(MODULE_NAME).getDocument(CORE.getUser().getCustomer(), DOCUMENT_NAME).newInstance(CORE.getUser());
 		}
@@ -219,7 +311,7 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 	@XmlTransient
 	public String getBizKey() {
 		try {
-			return org.skyve.util.Binder.formatMessage("Inline Grid", this);
+			return org.skyve.util.Binder.formatMessage("{text}", this);
 		}
 		catch (@SuppressWarnings("unused") Exception e) {
 			return "Unknown";
@@ -228,8 +320,8 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 
 	@Override
 	public boolean equals(Object o) {
-		return ((o instanceof InlineGrid) && 
-					this.getBizId().equals(((InlineGrid) o).getBizId()));
+		return ((o instanceof ListAttributes) && 
+					this.getBizId().equals(((ListAttributes) o).getBizId()));
 	}
 
 	/**
@@ -361,6 +453,64 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 	}
 
 	/**
+	 * {@link #dateTime} accessor.
+	 * @return	The value.
+	 **/
+	public DateTime getDateTime() {
+		return dateTime;
+	}
+
+	/**
+	 * {@link #dateTime} mutator.
+	 * @param dateTime	The new value.
+	 **/
+	@XmlElement
+	@XmlSchemaType(name = "dateTime")
+	@XmlJavaTypeAdapter(DateTimeMapper.class)
+	public void setDateTime(DateTime dateTime) {
+		preset(dateTimePropertyName, dateTime);
+		this.dateTime = dateTime;
+	}
+
+	/**
+	 * {@link #decimal10} accessor.
+	 * @return	The value.
+	 **/
+	public Decimal10 getDecimal10() {
+		return decimal10;
+	}
+
+	/**
+	 * {@link #decimal10} mutator.
+	 * @param decimal10	The new value.
+	 **/
+	@XmlElement
+	@XmlJavaTypeAdapter(Decimal10Mapper.class)
+	public void setDecimal10(Decimal10 decimal10) {
+		preset(decimal10PropertyName, decimal10);
+		this.decimal10 = decimal10;
+	}
+
+	/**
+	 * {@link #decimal2} accessor.
+	 * @return	The value.
+	 **/
+	public Decimal2 getDecimal2() {
+		return decimal2;
+	}
+
+	/**
+	 * {@link #decimal2} mutator.
+	 * @param decimal2	The new value.
+	 **/
+	@XmlElement
+	@XmlJavaTypeAdapter(Decimal2Mapper.class)
+	public void setDecimal2(Decimal2 decimal2) {
+		preset(decimal2PropertyName, decimal2);
+		this.decimal2 = decimal2;
+	}
+
+	/**
 	 * {@link #decimal5} accessor.
 	 * @return	The value.
 	 **/
@@ -379,29 +529,170 @@ public class InlineGrid extends AbstractTransientBean implements ChildBean<Kitch
 		this.decimal5 = decimal5;
 	}
 
-	@Override
-	public KitchenSink getParent() {
-		return parent;
+	/**
+	 * {@link #id} accessor.
+	 * @return	The value.
+	 **/
+	public String getId() {
+		return id;
 	}
 
-	@Override
+	/**
+	 * {@link #id} mutator.
+	 * @param id	The new value.
+	 **/
 	@XmlElement
-	public void setParent(KitchenSink parent) {
-		if (this.parent != parent) {
-			preset(ChildBean.PARENT_NAME, parent);
-			this.parent = parent;
-		}
+	public void setId(String id) {
+		preset(idPropertyName, id);
+		this.id = id;
 	}
 
-	@Override
-	public Integer getBizOrdinal() {
-		return bizOrdinal;
+	/**
+	 * {@link #normalInteger} accessor.
+	 * @return	The value.
+	 **/
+	public Integer getNormalInteger() {
+		return normalInteger;
 	}
 
-	@Override
+	/**
+	 * {@link #normalInteger} mutator.
+	 * @param normalInteger	The new value.
+	 **/
 	@XmlElement
-	public void setBizOrdinal(Integer bizOrdinal) {
-		preset(Bean.ORDINAL_NAME, bizOrdinal);
-		this.bizOrdinal =  bizOrdinal;
+	public void setNormalInteger(Integer normalInteger) {
+		preset(normalIntegerPropertyName, normalInteger);
+		this.normalInteger = normalInteger;
+	}
+
+	/**
+	 * {@link #longInteger} accessor.
+	 * @return	The value.
+	 **/
+	public Long getLongInteger() {
+		return longInteger;
+	}
+
+	/**
+	 * {@link #longInteger} mutator.
+	 * @param longInteger	The new value.
+	 **/
+	@XmlElement
+	public void setLongInteger(Long longInteger) {
+		preset(longIntegerPropertyName, longInteger);
+		this.longInteger = longInteger;
+	}
+
+	/**
+	 * {@link #markup} accessor.
+	 * @return	The value.
+	 **/
+	public String getMarkup() {
+		return markup;
+	}
+
+	/**
+	 * {@link #markup} mutator.
+	 * @param markup	The new value.
+	 **/
+	@XmlElement
+	public void setMarkup(String markup) {
+		preset(markupPropertyName, markup);
+		this.markup = markup;
+	}
+
+	/**
+	 * {@link #memo} accessor.
+	 * @return	The value.
+	 **/
+	public String getMemo() {
+		return memo;
+	}
+
+	/**
+	 * {@link #memo} mutator.
+	 * @param memo	The new value.
+	 **/
+	@XmlElement
+	public void setMemo(String memo) {
+		preset(memoPropertyName, memo);
+		this.memo = memo;
+	}
+
+	/**
+	 * {@link #text} accessor.
+	 * @return	The value.
+	 **/
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * {@link #text} mutator.
+	 * @param text	The new value.
+	 **/
+	@XmlElement
+	public void setText(String text) {
+		preset(textPropertyName, text);
+		this.text = text;
+	}
+
+	/**
+	 * {@link #time} accessor.
+	 * @return	The value.
+	 **/
+	public TimeOnly getTime() {
+		return time;
+	}
+
+	/**
+	 * {@link #time} mutator.
+	 * @param time	The new value.
+	 **/
+	@XmlElement
+	@XmlSchemaType(name = "time")
+	@XmlJavaTypeAdapter(TimeOnlyMapper.class)
+	public void setTime(TimeOnly time) {
+		preset(timePropertyName, time);
+		this.time = time;
+	}
+
+	/**
+	 * {@link #timestamp} accessor.
+	 * @return	The value.
+	 **/
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	/**
+	 * {@link #timestamp} mutator.
+	 * @param timestamp	The new value.
+	 **/
+	@XmlElement
+	@XmlSchemaType(name = "dateTime")
+	@XmlJavaTypeAdapter(TimestampMapper.class)
+	public void setTimestamp(Timestamp timestamp) {
+		preset(timestampPropertyName, timestamp);
+		this.timestamp = timestamp;
+	}
+
+	/**
+	 * condition
+	 *
+	 * @return The condition
+	 */
+	@XmlTransient
+	public boolean isCondition() {
+		return (true);
+	}
+
+	/**
+	 * {@link #isCondition} negation.
+	 *
+	 * @return The negated condition
+	 */
+	public boolean isNotCondition() {
+		return (! isCondition());
 	}
 }
