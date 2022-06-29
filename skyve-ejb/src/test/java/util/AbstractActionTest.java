c0d3ee38f53e9e3b95eb3c7d6ba227f2c710acbc
Ben Petito <benpetito@gmail.com>
2022-06-29 12:29:15 +0930

    Update AbstractActionTest to use the MockWebContext to avoid exceptions in tests when actions use a webContext

--- skyve-ejb/src/test/java/util/AbstractActionTest.java
+++ skyve-ejb/src/test/java/util/AbstractActionTest.java
@@ -3,6 +3,7 @@ package util;
 import org.junit.jupiter.api.Test;
 import org.skyve.domain.Bean;
 import org.skyve.domain.messages.ValidationException;
+import org.skyve.impl.sail.mock.MockWebContext;
 import org.skyve.metadata.controller.ServerSideAction;
 
 public abstract class AbstractActionTest<T extends Bean, A extends ServerSideAction<T>> extends AbstractH2TestForJUnit5 {
@@ -14,7 +15,7 @@ public abstract class AbstractActionTest<T extends Bean, A extends ServerSideAct
 	@Test
 	public void testExecute() throws Exception {
 		try {
-			getAction().execute(getBean(), null);
+			getAction().execute(getBean(), new MockWebContext());
 		} catch (ValidationException e) {
 			// test pass - action validated incorrect input
 		}
