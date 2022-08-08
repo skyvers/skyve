8f1d093533d44bedcb5703c60d95594c6df2e053
Brandon Klar <brandon.klar@bizhub.com.au>
2022-08-08 15:51:04 +0930

    update ModulesUtil configureGroup test to use created roles

--- skyve-ejb/src/test/java/modules/admin/ModulesUtilTest.java
+++ skyve-ejb/src/test/java/modules/admin/ModulesUtilTest.java
@@ -22,6 +22,7 @@ import modules.admin.Group.GroupExtension;
 import modules.admin.User.UserExtension;
 import modules.admin.domain.Contact;
 import modules.admin.domain.Group;
+import modules.admin.domain.GroupRole;
 import modules.admin.domain.User;
 import util.AbstractH2TestForJUnit5;
 
@@ -126,18 +127,27 @@ public class ModulesUtilTest extends AbstractH2TestForJUnit5 {
 
 	@Test
 	void configureGroup() {
+		// Create test roles 
+		GroupRole testRole1 = GroupRole.newInstance();
+		testRole1.setRoleName("admin.TestRole1");
+		assertThat(testRole1, is(notNullValue()));
+		
+		GroupRole testRole2 = GroupRole.newInstance();
+		testRole2.setRoleName("admin.TestRole2");
+		assertThat(testRole2, is(notNullValue()));
+		
 		// New Group
-		GroupExtension newGroup = ModulesUtil.configureGroup("TestGroup", "admin.BasicUser", "admin.ContactManager");
+		GroupExtension newGroup = ModulesUtil.configureGroup("TestGroup", "admin.TestRole1", "admin.TestRole2");
 		assertThat(newGroup, is(notNullValue()));
 		
 		// Attempting to create new group with name and roles of existing group
-		GroupExtension testExistingGroup = ModulesUtil.configureGroup("TestGroup", "admin.BasicUser", "admin.ContactManager");
+		GroupExtension testExistingGroup = ModulesUtil.configureGroup("TestGroup", "admin.TestRole1", "admin.TestRole2");
 		assertThat(testExistingGroup, is(notNullValue()));
 		assertThat(testExistingGroup, is(newGroup));
 
 		// Attempting to create new group with name of existing group but missing role
-		GroupExtension testExistingGroupWithMissingRole = ModulesUtil.configureGroup("TestGroup", "admin.ContactManager");
-		assertThat(testExistingGroup, is(notNullValue()));
-		assertThat(testExistingGroup, is(testExistingGroupWithMissingRole));
+		GroupExtension testExistingGroupWithMissingRole = ModulesUtil.configureGroup("TestGroup", "admin.TestRole1");
+		assertThat(testExistingGroupWithMissingRole, is(notNullValue()));
+		assertThat(testExistingGroupWithMissingRole, is(testExistingGroup));
 	}
 }
