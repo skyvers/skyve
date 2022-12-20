0fde82ac58bdd3835e3e436304bfbf9520de276b
Mike Sands <mike.sands@bizhub.com.au>
2022-12-20 13:59:33 +1030

    Add stub hbox implementation.

--- skyve-core/src/main/resources/org/skyve/impl/generate/client/flutter/lib/widgets/skyve_border.dart
+++ skyve-core/src/main/resources/org/skyve/impl/generate/client/flutter/lib/widgets/skyve_border.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-class SkyveBorder extends Container {
+class SkyveBorder extends Card {
   SkyveBorder({Key? key, String? title, Widget? child})
       : super(
             key: key,
-            child: child,
-            decoration: BoxDecoration(border: Border.all()),
+            child: (title == null)
+                ? child
+                : Column(children: [ListTile(title: Text(title!)), child!]),
             margin: const EdgeInsets.all(8.0));
 }
--- skyve-core/src/main/resources/org/skyve/impl/generate/client/flutter/lib/widgets/skyve_hbox.dart
+++ skyve-core/src/main/resources/org/skyve/impl/generate/client/flutter/lib/widgets/skyve_hbox.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 
-class SkyveHBox extends Wrap {
-  SkyveHBox({Key? key, List<Widget> children = const []})
-      : super(key: key, children: children);
+class SkyveHBox extends StatelessWidget {
+  final List<Widget> children;
+
+  const SkyveHBox({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<BootstrapCol> cols = List.empty(growable: true);
+    for (Widget child in children) {
+      final BootstrapCol col = BootstrapCol(
+          absoluteSizes: false,
+          sizes: "col-12 col-md-3",
+          child: child);
+      cols.add(col);
+    }
+    return BootstrapContainer(children: [BootstrapRow(children: cols)]);
+  }
 }
