2bab4f7cb120600742c3224f84c18efedb3cd38c
Mat Forest <mat@forest.id.au>
2022-12-22 12:12:51 +1030

    [login] Use cookies for login, rest client clean-up

--- skyve-core/src/main/resources/org/skyve/impl/generate/client/flutter/lib/util/skyve_rest_client.dart
+++ skyve-core/src/main/resources/org/skyve/impl/generate/client/flutter/lib/util/skyve_rest_client.dart
@@ -1,157 +1,147 @@
 import 'dart:convert';
-import 'dart:io';
 
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class SkyveRestClient {
-  static const int ok = 200;
-
   /// Set this value using --dart-define=SERVER_URL=http://xyz/etc/ when launching
-  static const _baseUri =
-      'http://localhost:8080/skyve/'; //String.fromEnvironment('SERVER_URL');
+  static const _baseUri = 'http://localhost:8080/skyve/';
+  // static const _baseUri = 'http://10.0.2.2:8080/skyve/';
 
-  /// Secure key-value store from flutter_secure_storage
-  static const _storage = FlutterSecureStorage();
-  static const _authStorageKey = 'Authorization';
+  static final instance = SkyveRestClient._internal();
+  bool _loggedIn = false;
 
-  final Dio _dio = Dio();
+  final Dio _dio = Dio(BaseOptions(
+    baseUrl: _baseUri,
+  ));
 
-  SkyveRestClient() {
-    // dio options, interceptors, etc
+  factory SkyveRestClient() {
+    return instance;
+  }
+
+  SkyveRestClient._internal() {
     _dio.options.connectTimeout = 10000;
+
+    // Manage cookies
+    final cookieJar = CookieJar();
+    _dio.interceptors.add(CookieManager(cookieJar));
+
+    // Log requests
+    // _dio.interceptors.add(PrettyDioLogger(
+    //     requestHeader: true,
+    //     requestBody: true,
+    //     responseBody: true,
+    //     responseHeader: true,
+    //     error: true,
+    //     compact: false,
+    //     maxWidth: 90));
   }
 
   String getBaseUri() {
-    // if (kIsWeb) {
-    //   return '../api/';
-    // }
-
-    // return _baseUri + "api/";
     return _baseUri;
   }
 
-  Future<http.Response> _fetch(String url, String body) async {
-    final String uri = getBaseUri() + url;
-    debugPrint(uri);
-    final Map<String, String> headers = {"Authorization": await _authHeader()};
+  bool get loggedIn {
+    return _loggedIn;
+  }
 
-    final response =
-        // await http.post(Uri.parse(uri), headers: headers, body: body);
-        await http.get(Uri.parse(uri), headers: headers);
+  Future<String> _fetch(String url) async {
+    final Response<String> response = await _dio.get(url);
+    if (response.data == null) {
+      throw Exception('Response data was null');
+    }
 
-    if (response.statusCode == ok) {
-      return response;
-    } else {
-      throw Exception('Request failed statusCode=[${response.statusCode}]');
+    return response.data!;
+  }
+
+  /// Attempt to login using the provided creds
+  Future<bool> login(
+      {required String username,
+      required String password,
+      String customer = 'default'}) async {
+    bool result = await _doLogin(
+        username: username, password: password, customer: customer);
+
+    _loggedIn = result;
+    return result;
+  }
+
+  Future<bool> _doLogin(
+      {required String username,
+      required String password,
+      required String customer}) async {
+    final params = {'password': password, 'username': '$customer/$username'};
+    validateStatus(int? status) {
+      return status != null && status >= 200 && status < 300;
+    }
+
+    /// In browser the 302 response will be followed without
+    /// any chance of us touching it. On mobile the redirect
+    /// will not be followed because we're doing a POST.
+    try {
+      Response<String> response = await _dio.post('loginAttempt',
+          queryParameters: params,
+          options: Options(validateStatus: validateStatus));
+      // This'll throw an error when running anywhere except the browser
+
+      // Big ol' hack here:
+      // Search for this title string in the response,
+      // this one is the /login?error page
+      String responseContent = response.data ?? '';
+      int idx = responseContent.indexOf('<title>Skyve: Sign In</title>');
+
+      if (idx == -1) {
+        return true;
+      } else {
+        return false;
+      }
+    } on DioError catch (err) {
+      // On Android etc, we'll end up here
+
+      // This URI thing is very dumb....
+      const failureUriFragment = 'login?error';
+      final locationHeader = err.response?.headers['location'];
+
+      if (locationHeader == null || locationHeader.length != 1) {
+        // Header wasn't present, or had more than one result
+        return false;
+      }
+
+      // If the location header contains 'login?error' we haven't
+      // logged in sucessfully
+      return (!locationHeader[0].contains(failureUriFragment));
     }
   }
 
   Future<List<dynamic>> query(
       String moduleName, String queryName, int startRow, endRow) async {
     debugPrint('Fetch list $moduleName.$queryName');
+
     return await _fetch(
-            'smartlist?_operationType=fetch&_dataSource=${moduleName}_$queryName&_startRow=$startRow&_endRow=$endRow',
-            '')
-        .then((response) {
-      debugPrint(response.body);
-      return jsonDecode(response.body)['response']['data'];
+            'smartlist?_operationType=fetch&_dataSource=${moduleName}_$queryName&_startRow=$startRow&_endRow=$endRow')
+        .then((jsonString) {
+      var decoded = jsonDecode(jsonString);
+      return decoded['response']['data'];
     });
   }
 
   Future<Map<String, dynamic>> edit(
       String moduleName, String documentName, String? bizId) async {
     debugPrint('Edit bean $moduleName.$documentName#$bizId');
+
     String url =
         'smartedit?_operationType=fetch&_mod=$moduleName&_doc=$documentName&_ecnt=0&_ccnt=0';
+
     if (bizId != null) {
       url += '&bizId=$bizId';
     }
-    return await _fetch(url, '').then((response) {
-      debugPrint(response.body);
-      return jsonDecode(response.body)['response']['data'][0];
+
+    return await _fetch(url).then((jsonString) {
+      return jsonDecode(jsonString)['response']['data'][0];
     });
-  }
-
-  Future<String> _authHeader() async {
-/*
-    var authHeader = await _storage.read(key: _authStorageKey);
-
-    if (authHeader == null) {
-      throw Exception('Stored Authorization was not found');
-    }
-
-    return authHeader;
-*/
-    return 'Basic ${base64.encode(utf8.encode('demo/admin:admin'))}';
-  }
-
-  /// Try to use the stored credentials to "log in", throws an error
-  /// if there are no stored creds or the creds are not valid
-  Future<void> tryLogin() async {
-    final String uri = '${getBaseUri()}submission';
-    final headers = {"Authorization": await _authHeader()};
-
-    final response = await http.get(Uri.parse(uri), headers: headers);
-    if (response.statusCode == ok) {
-      return;
-    }
-
-    throw Exception('Unable to use stored credentials');
-  }
-
-  Future<Result> loginWithCredentials(
-      String customer, String username, String password) async {
-    String credentials = '$customer/$username:$password';
-    // If not web, check if phone is connected
-    if (!kIsWeb) {
-      try {
-        final result = await InternetAddress.lookup('example.com');
-        if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-          debugPrint('Connected');
-        }
-      } on SocketException catch (_) {
-        debugPrint('Not Connected');
-        return Result(true, 'Check your phone connectivity.');
-      }
-    }
-
-    String authHeader = 'Basic ${base64.encode(utf8.encode(credentials))}';
-    // Call a skyve API, and on success persist the Authorization details
-    final String uri = '${getBaseUri()}submission';
-    final headers = {"Authorization": authHeader};
-
-    // FIXME need to shorten timeout, in case server is unreachable
-    // currently it's 60s (I think)
-
-    try {
-      debugPrint('Attempting login with URI: $uri');
-
-      final response = await http.get(Uri.parse(uri), headers: headers);
-
-      if (response.statusCode == ok) {
-        await _saveAuthorization(authHeader);
-
-        return Result(false, 'no error');
-      } else {
-        return Result(true, 'Unknown error ${response.statusCode}',
-            statusCode: response.statusCode);
-      }
-    } catch (ex) {
-      debugPrint('Login attempt failed; $ex');
-      return Result(true, ex.toString());
-    }
-  }
-
-  Future<void> _saveAuthorization(String authHeaderValue) async {
-    await _storage.write(key: _authStorageKey, value: authHeaderValue);
-  }
-
-  /// Clear stored credentials
-  Future<void> clearAuthorization() async {
-    await _storage.delete(key: _authStorageKey);
   }
 }
 
