04b4c322f1f2e1ff17e6136bccd1b9c29ca7e50b
Brandon Klar <brandon.klar@bizhub.com.au>
2023-02-03 12:24:58 +1030

    add CommunicationBizletTest and a test for communicationExistsAnonymously

---
+++ skyve-ejb/src/test/java/modules/admin/Communication/CommunicationBizletTest.java
@@ -0,0 +1,37 @@
+package modules.admin.Communication;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+import org.skyve.CORE;
+import org.skyve.persistence.Persistence;
+import org.skyve.util.DataBuilder;
+import org.skyve.util.test.SkyveFixture;
+
+import modules.admin.domain.Communication;
+import util.AbstractH2Test;
+
+public class CommunicationBizletTest extends AbstractH2Test {
+
+	@SuppressWarnings({ "deprecation", "static-method" })
+	@Test
+	public void testAnonymouslyCommunicationExists() {
+		// create the test data
+		Persistence pers = CORE.getPersistence();
+		Communication c = new DataBuilder().fixture(SkyveFixture.FixtureType.crud).build(Communication.MODULE_NAME,
+				Communication.DOCUMENT_NAME);
+		c = pers.save(c);
+		String customer = pers.getUser().getCustomerName();
+
+		// call the method under test
+		boolean result = CommunicationBizlet.anonymouslyCommunicationExists(pers, customer, c.getBizId());
+
+		// verify the result
+		assertThat(Boolean.valueOf(result), is(notNullValue()));
+		assertTrue(result);
+	}
+
+}
