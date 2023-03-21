25ac0dce7118b516dccaad4a843663868757b3d4
Mike Sands <mike.sands@bizhub.com.au>
2023-03-21 11:47:48 +1030

    Extends now implements SerializableMetaData.

--- skyve-core/src/main/java/org/skyve/metadata/model/Extends.java
+++ skyve-core/src/main/java/org/skyve/metadata/model/Extends.java
@@ -6,10 +6,13 @@ import javax.xml.bind.annotation.XmlType;
 
 import org.skyve.impl.util.UtilImpl;
 import org.skyve.impl.util.XMLMetaData;
+import org.skyve.metadata.SerializableMetaData;
 
 @XmlRootElement(namespace = XMLMetaData.DOCUMENT_NAMESPACE)
 @XmlType(namespace = XMLMetaData.DOCUMENT_NAMESPACE)
-public class Extends {
+public class Extends implements SerializableMetaData {
+	private static final long serialVersionUID = -7377979289426429434L;
+	
 	private String documentName;
  	public String getDocumentName() {
         return documentName;
