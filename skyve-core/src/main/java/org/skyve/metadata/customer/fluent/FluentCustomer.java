1f10695c636108ef61849fd009fef7409a13fba3
Mike Sands <mike.sands@bizhub.com.au>
2024-03-26 11:36:07 +1030

    Add textSearch, flag and switchMode role management to FluentCustomer.

--- skyve-core/src/main/java/org/skyve/metadata/customer/fluent/FluentCustomer.java
+++ skyve-core/src/main/java/org/skyve/metadata/customer/fluent/FluentCustomer.java
@@ -1,5 +1,6 @@
 package org.skyve.metadata.customer.fluent;
 
+import org.skyve.impl.metadata.repository.customer.CustomerFeatureRoleMetaData;
 import org.skyve.impl.metadata.repository.customer.CustomerMetaData;
 import org.skyve.impl.metadata.repository.customer.HTMLResourcesMetaData;
 import org.skyve.impl.metadata.repository.customer.InterceptorMetaDataImpl;
@@ -10,6 +11,9 @@ import org.skyve.metadata.customer.Customer;
 import org.skyve.metadata.customer.InterceptorMetaData;
 import org.skyve.metadata.customer.ObserverMetaData;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+
 public class FluentCustomer {
 	private CustomerMetaData customer = null;
 	
@@ -17,11 +21,11 @@ public class FluentCustomer {
 		customer = new CustomerMetaData();
 	}
 	
-	public FluentCustomer(CustomerMetaData customer) {
+	public FluentCustomer(@Nonnull CustomerMetaData customer) {
 		this.customer = customer;
 	}
 
-	public FluentCustomer from(@SuppressWarnings("hiding") Customer customer) {
+	public @Nonnull FluentCustomer from(@SuppressWarnings("hiding") @Nonnull Customer customer) {
 		name(customer.getName());
 		language(customer.getLanguageTag());
 
@@ -38,6 +42,9 @@ public class FluentCustomer {
 
 		// Populate module names
 		modules(new FluentCustomerModules().from(customer.getModules(), customer.getHomeModule()));
+
+		// Populate Roles
+		roles(new FluentCustomerRoles().from(customer.getRoles(), customer.isAllowModuleRoles()));
 
 		// Populate Roles
 		roles(new FluentCustomerRoles().from(customer.getRoles(), customer.isAllowModuleRoles()));
@@ -59,110 +66,218 @@ public class FluentCustomer {
 		return this;
 	}
 	
-	public FluentCustomer name(String name) {
+	public @Nonnull FluentCustomer name(@Nonnull String name) {
 		customer.setName(name);
 		return this;
 	}
 
-	public FluentCustomer language(String language) {
+	public @Nonnull FluentCustomer language(@Nullable String language) {
 		customer.setLanguage(language);
 		return this;
 	}
 	
-	public FluentCustomer logoRelativeFileName(String logo) {
+	public @Nonnull FluentCustomer logoRelativeFileName(@Nullable String logo) {
 		UIResources resources = new UIResources();
 		resources.setLogoRelativeFileName(logo);
 		customer.setUiResources(resources);
 		return this;
 	}
 	
-	public FluentCustomer cssRelativeFileName(String css) {
+	public @Nonnull FluentCustomer cssRelativeFileName(@Nullable String css) {
 		HTMLResourcesMetaData resources = new HTMLResourcesMetaData();
 		resources.setCssRelativeFileName(css);
 		customer.setHtmlResources(resources);
 		return this;
 	}
 
-	public FluentCustomer loginResource(FluentLoginResources resources) {
+	public @Nonnull FluentCustomer loginResource(@Nonnull FluentLoginResources resources) {
 		customer.setLoginResources(resources.get());
 		return this;
 	}
 	
-	public FluentCustomer defaultDateConverter(ConverterName converter) {
+	public @Nonnull FluentCustomer defaultDateConverter(@Nonnull ConverterName converter) {
 		customer.setDefaultDateConverter(converter);
 		return this;
 	}
 	
-	public FluentCustomer defaultDateTimeConverter(ConverterName converter) {
+	public @Nonnull FluentCustomer defaultDateTimeConverter(@Nonnull ConverterName converter) {
 		customer.setDefaultDateTimeConverter(converter);
 		return this;
 	}
 	
-	public FluentCustomer defaultTimeConverter(ConverterName converter) {
+	public @Nonnull FluentCustomer defaultTimeConverter(@Nonnull ConverterName converter) {
 		customer.setDefaultTimeConverter(converter);
 		return this;
 	}
 	
-	public FluentCustomer defaultTimestampConverter(ConverterName converter) {
+	public @Nonnull FluentCustomer defaultTimestampConverter(@Nonnull ConverterName converter) {
 		customer.setDefaultTimestampConverter(converter);
 		return this;
 	}
 	
-	public FluentCustomer modules(FluentCustomerModules modules) {
+	public @Nonnull FluentCustomer modules(@Nonnull FluentCustomerModules modules) {
 		customer.setModules(modules.get());
 		return this;
 	}
 	
-	public FluentCustomer addInterceptor(String fullyQualifiedClassName) {
+	public @Nonnull FluentCustomer roles(@Nonnull FluentCustomerRoles roles) {
+		customer.setRoles(roles.get());
+		return this;
+	}
+
+	public @Nonnull FluentCustomer addTextSearchRole(@Nonnull FluentCustomerFeatureRole role) {
+		customer.getTextSearchRoles().add(role.get());
+		return this;
+	}
+
+	public @Nonnull FluentCustomer removeTextSearchModuleRole(@Nonnull String moduleName, @Nonnull String name) {
+		customer.getTextSearchRoles().removeIf(r -> moduleName.equals(r.getModuleName()) && name.equals(r.getName()));
+		return this;
+	}
+
+	public @Nonnull FluentCustomer removeTextSearchCustomerRole(@Nonnull String name) {
+		customer.getTextSearchRoles().removeIf(r -> (r.getModuleName() == null) && name.equals(r.getName()));
+		return this;
+	}
+
+	public @Nonnull FluentCustomer clearTextSearchRoles() {
+		customer.getTextSearchRoles().clear();
+		return this;
+	}
+	
+	public @Nonnull FluentCustomerFeatureRole findTextSearchModuleRole(@Nonnull String moduleName, @Nonnull String name) {
+		CustomerFeatureRoleMetaData result = customer.getTextSearchRoles().stream().filter(r -> moduleName.equals(r.getModuleName()) && name.equals(r.getName())).findAny().orElse(null);
+		if (result != null) {
+			return new FluentCustomerFeatureRole(result);
+		}
+		return null;
+	}
+
+	public @Nonnull FluentCustomerFeatureRole findTextSearchCustomerRole(@Nonnull String name) {
+		CustomerFeatureRoleMetaData result = customer.getTextSearchRoles().stream().filter(r -> (r.getModuleName() == null) && name.equals(r.getName())).findAny().orElse(null);
+		if (result != null) {
+			return new FluentCustomerFeatureRole(result);
+		}
+		return null;
+	}
+
+	public @Nonnull FluentCustomer addFlagRole(@Nonnull FluentCustomerFeatureRole role) {
+		customer.getFlagRoles().add(role.get());
+		return this;
+	}
+
+	public @Nonnull FluentCustomer removeFlagModuleRole(@Nonnull String moduleName, @Nonnull String name) {
+		customer.getFlagRoles().removeIf(r -> moduleName.equals(r.getModuleName()) && name.equals(r.getName()));
+		return this;
+	}
+
+	public @Nonnull FluentCustomer removeFlagCustomerRole(@Nonnull String name) {
+		customer.getFlagRoles().removeIf(r -> (r.getModuleName() == null) && name.equals(r.getName()));
+		return this;
+	}
+
+	public @Nonnull FluentCustomer clearFlagRoles() {
+		customer.getFlagRoles().clear();
+		return this;
+	}
+	
+	public @Nullable FluentCustomerFeatureRole findFlagModuleRole(@Nonnull String moduleName, @Nonnull String name) {
+		CustomerFeatureRoleMetaData result = customer.getFlagRoles().stream().filter(r -> moduleName.equals(r.getModuleName()) && name.equals(r.getName())).findAny().orElse(null);
+		if (result != null) {
+			return new FluentCustomerFeatureRole(result);
+		}
+		return null;
+	}
+
+	public @Nullable FluentCustomerFeatureRole findFlagCustomerRole(@Nonnull String name) {
+		CustomerFeatureRoleMetaData result = customer.getFlagRoles().stream().filter(r -> (r.getModuleName() == null) && name.equals(r.getName())).findAny().orElse(null);
+		if (result != null) {
+			return new FluentCustomerFeatureRole(result);
+		}
+		return null;
+	}
+
+	public @Nonnull FluentCustomer addSwitchModeRole(@Nonnull FluentCustomerFeatureRole role) {
+		customer.getSwitchModeRoles().add(role.get());
+		return this;
+	}
+
+	public @Nonnull FluentCustomer removeSwitchModeModuleRole(@Nonnull String moduleName, @Nonnull String name) {
+		customer.getSwitchModeRoles().removeIf(r -> moduleName.equals(r.getModuleName()) && name.equals(r.getName()));
+		return this;
+	}
+
+	public @Nonnull FluentCustomer removeSwitchModeCustomerRole(@Nonnull String name) {
+		customer.getSwitchModeRoles().removeIf(r -> (r.getModuleName() == null) && name.equals(r.getName()));
+		return this;
+	}
+
+	public @Nonnull FluentCustomer clearSwitchModeRoles() {
+		customer.getTextSearchRoles().clear();
+		return this;
+	}
+	
+	public @Nullable FluentCustomerFeatureRole findSwitchModeModuleRole(@Nonnull String moduleName, @Nonnull String name) {
+		CustomerFeatureRoleMetaData result = customer.getSwitchModeRoles().stream().filter(r -> moduleName.equals(r.getModuleName()) && name.equals(r.getName())).findAny().orElse(null);
+		if (result != null) {
+			return new FluentCustomerFeatureRole(result);
+		}
+		return null;
+	}
+
+	public @Nullable FluentCustomerFeatureRole findSwitchModeCustomerRole(@Nonnull String name) {
+		CustomerFeatureRoleMetaData result = customer.getSwitchModeRoles().stream().filter(r -> (r.getModuleName() == null) && name.equals(r.getName())).findAny().orElse(null);
+		if (result != null) {
+			return new FluentCustomerFeatureRole(result);
+		}
+		return null;
+	}
+
+	public @Nonnull FluentCustomer addInterceptor(@Nonnull String fullyQualifiedClassName) {
 		InterceptorMetaDataImpl interceptor = new InterceptorMetaDataImpl();
 		interceptor.setClassName(fullyQualifiedClassName);
 		customer.getInterceptors().add(interceptor);
 		return this;
 	}
 	
-	public FluentCustomer removeInterceptor(String fullyQualifiedClassName) {
+	public @Nonnull FluentCustomer removeInterceptor(@Nonnull String fullyQualifiedClassName) {
 		customer.getInterceptors().removeIf(i -> fullyQualifiedClassName.equals(i.getClassName()));
 		return this;
 	}
 	
-	public FluentCustomer clearInterceptors() {
+	public @Nonnull FluentCustomer clearInterceptors() {
 		customer.getInterceptors().clear();
 		return this;
 	}
 	
-	public FluentCustomer addObserver(String fullyQualifiedClassName) {
+	public @Nonnull FluentCustomer addObserver(@Nonnull String fullyQualifiedClassName) {
 		ObserverMetaDataImpl observer = new ObserverMetaDataImpl();
 		observer.setClassName(fullyQualifiedClassName);
 		customer.getObservers().add(observer);
 		return this;
 	}
 	
-	public FluentCustomer removeObserver(String fullyQualifiedClassName) {
+	public @Nonnull FluentCustomer removeObserver(@Nonnull String fullyQualifiedClassName) {
 		customer.getObservers().removeIf(o -> fullyQualifiedClassName.equals(o.getClassName()));
 		return this;
 	}
 	
-	public FluentCustomer clearObservers() {
+	public @Nonnull FluentCustomer clearObservers() {
 		customer.getObservers().clear();
 		return this;
 	}
 	
-	public FluentCustomer roles(FluentCustomerRoles roles) {
-		customer.setRoles(roles.get());
-		return this;
-	}
-
-	public FluentCustomer jFreeChartPostProcessorClassName(String fullyQualifiedClassName) {
+	public @Nonnull FluentCustomer jFreeChartPostProcessorClassName(@Nonnull String fullyQualifiedClassName) {
 		customer.setJFreeChartPostProcessorClassName(fullyQualifiedClassName);
 		return this;
 	}
 	
-	public FluentCustomer primeFacesChartPostProcessorClassName(String fullyQualifiedClassName) {
+	public @Nonnull FluentCustomer primeFacesChartPostProcessorClassName(@Nonnull String fullyQualifiedClassName) {
 		customer.setPrimeFacesChartPostProcessorClassName(fullyQualifiedClassName);
 		return this;
 	}
 
-	public CustomerMetaData get() {
+	public @Nonnull CustomerMetaData get() {
 		return customer;
 	}
 }
