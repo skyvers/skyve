5dc2d6a148ba7e3bde4be9905f288da4a97cff39
Mat Forest <mat@forest.id.au>
2024-05-28 16:04:07 +0930

    [vue] Fix for grid horizontal overflow in component builder
    Immediately below the #sidebarWrapper div the ResponsiveComponentBuilder
    drops this div with `flex-grow:1` I've added `max-width:100%` so that
    the VueListGrid won't overflow on narrower screens.

--- skyve-web/src/main/java/org/skyve/impl/web/faces/pipeline/component/ResponsiveComponentBuilder.java
+++ skyve-web/src/main/java/org/skyve/impl/web/faces/pipeline/component/ResponsiveComponentBuilder.java
@@ -37,7 +37,7 @@ public class ResponsiveComponentBuilder extends TabularComponentBuilder {
 		HtmlPanelGroup result = (HtmlPanelGroup) super.view(component, createView);
 		// Change from a span with nowrap to a responsive div
 		result.setLayout("block");
-		result.setStyle("flex-grow:1"); // to ensure it takes up all the space it can in the sidebarWrapper div
+		result.setStyle("flex-grow:1; max-width:100%"); // to ensure it takes up all the space it can in the sidebarWrapper div
 		result.setStyleClass(UtilImpl.PRIMEFLEX ? "p-grid" : "ui-g");
 
 		return result;
