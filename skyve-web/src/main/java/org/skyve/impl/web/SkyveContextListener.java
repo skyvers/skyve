9ed754734ce9c1f3c6dd8f0719ff9425fea6c446
Simeon Solomou <simeon.solomou@bizhub.com.au>
2024-08-27 10:53:43 +0930

    Use Boolean.TRUE.equals rather than try-catch for optional json property

--- skyve-web/src/main/java/org/skyve/impl/web/SkyveContextListener.java
+++ skyve-web/src/main/java/org/skyve/impl/web/SkyveContextListener.java
@@ -738,11 +738,7 @@ public class SkyveContextListener implements ServletContextListener {
 		}
 
 		Map<String, Object> api = getObject(null, "api", properties, true);
-		try {
-			UtilImpl.CHECK_FOR_BREACHED_PASSWORD = getBoolean("api", "checkForBreachedPassword", api);
-		} catch (@SuppressWarnings("unused") Exception e) {
-			// Ignore - optional property in JSON
-		}
+		UtilImpl.CHECK_FOR_BREACHED_PASSWORD = Boolean.TRUE.equals(get("api", "checkForBreachedPassword", api, false));
 		UtilImpl.GOOGLE_MAPS_V3_API_KEY = getString("api", "googleMapsV3Key", api, false);
 		UtilImpl.GOOGLE_RECAPTCHA_SITE_KEY = getString("api", "googleRecaptchaSiteKey", api, false);
 		UtilImpl.GOOGLE_RECAPTCHA_SECRET_KEY = getString("api", "googleRecaptchaSecretKey", api, false);
