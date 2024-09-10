c5b461779ce4215a98fa1b4c88b2317b79561dc7
Simeon Solomou <simeon.solomou@bizhub.com.au>
2024-09-10 13:35:24 +0930

    Revert "Fixed BackupJob.getSensitivityLevel"
    This reverts commit fb5c61a048b5863102d97e1f4518a97cf34e506a.

--- skyve-ext/src/main/java/org/skyve/impl/backup/BackupJob.java
+++ skyve-ext/src/main/java/org/skyve/impl/backup/BackupJob.java
@@ -519,15 +519,11 @@ public class BackupJob extends CancellableJob {
 	 * @param bean DataMaintenance bean
 	 */
 	private static int getSensitivityLevel(Bean bean) {
-		DataSensitivity sensitivityInput = null;
-		try {
-			sensitivityInput = (DataSensitivity) Binder.get(bean, AppConstants.DATA_SENSITIVITY_ATTRIBUTE_NAME);
-		} catch (Exception e) {
-			Util.LOGGER.warning("Failed to get sensitivity input");
-			e.printStackTrace();
-		}
-		if (sensitivityInput != null) {
-			return Sensitivity.valueOf(sensitivityInput.toString()).ordinal();
+		if (bean instanceof DataMaintenance dataMaintenance) {
+			DataSensitivity sensitivityInput = dataMaintenance.getDataSensitivity();
+			if (sensitivityInput != null) {
+				return Sensitivity.valueOf(sensitivityInput.toString()).ordinal();
+			}
 		}
 		
 		return 0;
