ad14f730823c9eb1614859a6b666d57b764d92cf
Mike Sands <mike.sands@bizhub.com.au>
2024-10-23 09:35:01 +1030

    Skyve 9.2.

--- skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
+++ skyve-core/src/main/java/org/skyve/impl/util/UtilImpl.java
@@ -75,7 +75,7 @@ public class UtilImpl {
 
 	// For versioning javascript/css etc for web site
 	public static final String WEB_RESOURCE_FILE_VERSION = "56";
-	public static final String SKYVE_VERSION = "9.2.0-SNAPSHOT";
+	public static final String SKYVE_VERSION = "9.2.0";
 	public static final String SMART_CLIENT_DIR = "isomorphic130";
 
 	public static boolean XML_TRACE = false;
