09be96c0068657da109e2bf6794a41c510de2b15
Malcolm-G-Biz <malcolm.githuka@bizhub.com.au>
2025-10-13 18:24:36 +0300

    Implement CommunicationService and refactor Bizlet to use it

--- skyve-war/src/test/java/modules/admin/Communication/CommunicationServiceTest.java
+++ skyve-war/src/test/java/modules/admin/Communication/CommunicationServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.skyve.CORE;
 import org.skyve.persistence.Persistence;
 import org.skyve.util.DataBuilder;
@@ -14,9 +15,17 @@ import org.skyve.util.test.SkyveFixture;
 import modules.admin.domain.Communication;
 import util.AbstractH2TestForJUnit4;
 
-public class CommunicationBizletTest extends AbstractH2TestForJUnit4 {
+public class CommunicationServiceTest extends AbstractH2TestForJUnit4 {
+	
+	private CommunicationService communicationService;
 
-	@SuppressWarnings({ "deprecation", "static-method" })
+	@BeforeEach
+	public void setup() throws Exception {
+		// Initialise CommunicationService
+		communicationService = new CommunicationService();
+	}
+
+	@SuppressWarnings({ "deprecation" })
 	@Test
 	public void testAnonymouslyCommunicationExists() {
 		// create the test data
@@ -25,9 +34,9 @@ public class CommunicationBizletTest extends AbstractH2TestForJUnit4 {
 				Communication.DOCUMENT_NAME);
 		c = pers.save(c);
 		String customer = pers.getUser().getCustomerName();
-
+		
 		// call the method under test
-		boolean result = CommunicationBizlet.anonymouslyCommunicationExists(pers, customer, c.getBizId());
+		boolean result = communicationService.anonymouslyCommunicationExists(pers, customer, c.getBizId());
 
 		// verify the result
 		assertThat(Boolean.valueOf(result), is(notNullValue()));
