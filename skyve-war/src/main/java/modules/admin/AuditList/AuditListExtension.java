03eb736517611177953eb87e9269b3c9740e26a6
Malcolm-G-Biz <141075400+Malcolm-G-Biz@users.noreply.github.com>
2025-10-15 16:25:40 +0300

    Audit document spruce up (#196)
    * Refactor bizlet methods into a service class
    
    * Clear warnings and update AuditList
    
    * PR feedback
    
    - Clean up some comments
    - Refactor to use service class in resolve method

--- skyve-war/src/main/java/modules/admin/AuditList/AuditListExtension.java
+++ skyve-war/src/main/java/modules/admin/AuditList/AuditListExtension.java
@@ -1,24 +1,18 @@
 package modules.admin.AuditList;
 
-import java.util.Optional;
-
-import org.skyve.impl.util.UtilImpl.ArchiveConfig.ArchiveDocConfig;
-import org.skyve.util.Util;
-
-import modules.admin.domain.Audit;
+import jakarta.inject.Inject;
+import modules.admin.Audit.AuditService;
 import modules.admin.domain.AuditList;
 
 public class AuditListExtension extends AuditList {
+	private static final long serialVersionUID = -2480022026425411282L;
+	
+	@Inject
+	private transient AuditService auditService;
 
     @Override
     public boolean isShowArchived() {
 
-        return auditDocConfig().isPresent();
-    }
-
-    private Optional<ArchiveDocConfig> auditDocConfig() {
-
-        return Util.getArchiveConfig()
-                   .findArchiveDocConfig(Audit.MODULE_NAME, Audit.DOCUMENT_NAME);
+        return auditService.auditDocConfig().isPresent();
     }
 }
