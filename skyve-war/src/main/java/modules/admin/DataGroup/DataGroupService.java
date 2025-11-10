d6b91f2f940dfd7fbb82919088fe7af19914e1d7
Malcolm-G-Biz <malcolm.githuka@bizhub.com.au>
2025-11-10 15:09:04 +0300

    Add DataGroupService class to encapsulate domain logic for data groups
    - move methods from DataGroupUtil to the service class

--- skyve-war/src/main/java/modules/admin/DataGroup/DataGroupService.java
+++ skyve-war/src/main/java/modules/admin/DataGroup/DataGroupService.java
@@ -1,9 +1,8 @@
-package modules.admin;
+package modules.admin.DataGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.skyve.CORE;
 import org.skyve.domain.Bean;
 import org.skyve.metadata.SortDirection;
 import org.skyve.metadata.customer.Customer;
@@ -15,13 +14,24 @@ import org.skyve.metadata.user.User;
 import org.skyve.persistence.DocumentQuery;
 import org.skyve.persistence.Persistence;
 import org.skyve.util.NullableBeanVisitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import jakarta.enterprise.inject.spi.CDI;
-import modules.admin.User.UserService;
+import jakarta.enterprise.inject.Default;
+import jakarta.inject.Inject;
 import modules.admin.domain.DataGroup;
 
-public class DataGroupUtil {
-
+/**
+ * This class acts as a service layer to encapsulate domain logic.
+ *
+ * Add this line to classes that wish to use it: @Inject private transient DataGroupService dataGroupService;
+ */
+@Default
+public class DataGroupService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DataGroupService.class);
+	@Inject
+	Persistence pers;
+	
 	/**
 	 * Iterate throughout all levels of the bean, modifying the datagroup to
 	 * that specified.
@@ -33,9 +43,9 @@ public class DataGroupUtil {
 	 * @throws Exception
 	 *         general Exception
 	 */
-	public static void publishToDataGroup(Bean b, DataGroup dg) throws Exception {
+	public void publishToDataGroup(Bean b, DataGroup dg) throws Exception {
 
-		Customer customer = CORE.getPersistence().getUser().getCustomer();
+		Customer customer = pers.getUser().getCustomer();
 		Document document = (customer.getModule(b.getBizModule())).getDocument(customer, b.getBizDocument());
 
 		System.out.println("For DataGroup" + dg.getName());
@@ -44,36 +54,30 @@ public class DataGroupUtil {
 			@Override
 			protected boolean acceptNulls(String binding, Document doc, Document owningDocument, Relation owningRelation, Bean bean)
 					throws Exception {
-				System.out.println("binding: " + binding);
+				LOGGER.info("binding: {}", binding);
 				return true;
 			}
 		}.visit(document, b, customer);
 	}
-
-	public static boolean currentAdminUserIsInDataGroup() {
-		return (CDI.current().select(UserService.class).get().currentAdminUser().getBizDataGroupId() != null);
-	}
-
+	
 	/**
 	 * Returns the list of data groups relevant for this customer
 	 * 
-	 * @param persistence
-	 * @return
-	 * @throws Exception
+	 * @param persistence the persistence context to use for database operations, or null to use the injected persistence
+	 * @return a list of DomainValue objects representing the data groups, ordered by name ascending
+	 * @throws Exception if an error occurs during database query or data retrieval
 	 */
-	public static List<DomainValue> getDataGroupDomainValues(Persistence persistence) throws Exception {
+	public List<DomainValue> getDataGroupDomainValues(Persistence persistence) throws Exception {
 
 		List<DomainValue> result = new ArrayList<>();
 
 		// use passed persistence if available
-		Persistence pers = null;
-		if (persistence == null) {
-			pers = CORE.getPersistence();
-		} else {
-			pers = persistence;
+		Persistence p = persistence;
+		if (p == null) {
+			p = pers;
 		}
 
-		DocumentQuery query = pers.newDocumentQuery(DataGroup.MODULE_NAME, DataGroup.DOCUMENT_NAME);
+		DocumentQuery query = p.newDocumentQuery(DataGroup.MODULE_NAME, DataGroup.DOCUMENT_NAME);
 		query.addBoundOrdering(DataGroup.namePropertyName, SortDirection.ascending);
 		List<DataGroup> groups = query.beanResults();
 		for (DataGroup group : groups) {
@@ -82,19 +86,18 @@ public class DataGroupUtil {
 
 		return result;
 	}
-
+	
 	/**
 	 * retrieve the data group for a given bean (from the BizDataGroupId)
 	 * 
-	 * @param bean
-	 * @return
-	 * @throws Exception
+	 * @param bean the bean object containing the BizDataGroupId to retrieve the data group for
+	 * @return the DataGroup object associated with the bean, or null if the bean has no BizDataGroupId
+	 * @throws Exception if an error occurs during data group retrieval or document access
 	 */
-	public static DataGroup getBeanDataGroup(Bean bean) throws Exception {
+	public DataGroup getBeanDataGroup(Bean bean) throws Exception {
 		DataGroup result = null;
 
 		if (bean.getBizDataGroupId() != null) {
-			Persistence pers = CORE.getPersistence();
 			User user = pers.getUser();
 			Customer customer = user.getCustomer();
 			Module module = customer.getModule(DataGroup.MODULE_NAME);
@@ -105,5 +108,4 @@ public class DataGroupUtil {
 
 		return result;
 	}
-
 }
