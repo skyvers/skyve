aab85fe72ac3b0785aab5bbde0534bf39aa4f964
Mike Sands <mike.sands@bizhub.com.au>
2026-05-18 11:12:55 +0930

    Fix InMemoryFilter.

--- skyve-core/src/main/java/org/skyve/metadata/view/model/list/InMemoryFilter.java
+++ skyve-core/src/main/java/org/skyve/metadata/view/model/list/InMemoryFilter.java
@@ -718,7 +718,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addWithin(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "within", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "within", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
@@ -730,7 +730,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addContains(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "contains", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "contains", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
@@ -742,7 +742,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addCrosses(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "crosses", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "crosses", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
@@ -754,7 +754,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addDisjoint(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "disjoint", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "disjoint", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
@@ -766,7 +766,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addIntersects(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "intersects", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "intersects", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
@@ -778,7 +778,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addOverlaps(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "overlaps", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "overlaps", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
@@ -790,7 +790,7 @@ public class InMemoryFilter implements Filter {
 
 	@Override
 	public void addTouches(String binding, Geometry value) {
-		predicates.add(new MyPredicate<Geometry>(binding, "touches", null, null, null) {
+		predicates.add(new MyPredicate<>(binding, "touches", value, null, null) {
 			@Override
 			@SuppressWarnings("hiding")
 			boolean evaluate(Object bean, String binding, Geometry value, Geometry start, Geometry end) throws Exception {
