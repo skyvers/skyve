61ce56fe59410214a56af0a2438f788c03b279f4
Mike Sands <mike.sands@bizhub.com.au>
2026-07-13 15:30:55 +0930

    Fix test.

--- skyve-web/src/test/java/org/skyve/impl/web/faces/pipeline/component/SkyveComponentBuilderChainTest.java
+++ skyve-web/src/test/java/org/skyve/impl/web/faces/pipeline/component/SkyveComponentBuilderChainTest.java
@@ -35,7 +35,7 @@ class SkyveComponentBuilderChainTest {
 	}
 
 	@Test
-	void listGridAppliesPaginationOnly() throws ReflectiveOperationException {
+	void listGridAppliesPaginationOnly() {
 		FacesContext facesContext = mock(FacesContext.class);
 		Application application = mock(Application.class);
 		ExpressionFactory expressionFactory = mock(ExpressionFactory.class);
@@ -47,18 +47,8 @@ class SkyveComponentBuilderChainTest {
 
 		SkyveComponentBuilderChain chain = new SkyveComponentBuilderChain();
 		DataTable dataTable = mock(DataTable.class);
-		java.lang.reflect.Method listGridMethod = null;
-		for (java.lang.reflect.Method method : ComponentBuilderChain.class.getMethods()) {
-			if ("listGrid".equals(method.getName()) && (method.getParameterCount() == 9)) {
-				listGridMethod = method;
-				break;
-			}
-		}
-		if (listGridMethod == null) {
-			throw new AssertionError("listGrid method not found");
-		}
 
-		assertSame(dataTable, listGridMethod.invoke(chain, dataTable, "mod", "doc", "model", "uxui", null, null, null, Boolean.FALSE));
+		assertSame(dataTable, chain.listGrid(dataTable, "mod", "doc", "model", "uxui", null, null, null, null, false));
 		verify(dataTable).setPaginator(true);
 		verify(dataTable).setRowsPerPageTemplate("25,50,75,100");
 		verify(dataTable).setPaginatorAlwaysVisible(false);
